916c16207f78e852d04b9c9b1ec539455be4d9e7
Robin Neatherway <robin.neatherway@gmail.com>
2015-05-31 01:35:04 +0100

    Bump version number for 0.17.0

--- FSharp.AutoComplete/AssemblyInfo.fs
+++ FSharp.AutoComplete/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.AutoComplete")>]
 [<assembly: AssemblyProductAttribute("FSharp.AutoComplete")>]
 [<assembly: AssemblyDescriptionAttribute("A command line tool for interfacing with FSharp.Compiler.Service over a pipe.")>]
-[<assembly: AssemblyVersionAttribute("0.16.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.16.0")>]
+[<assembly: AssemblyVersionAttribute("0.17.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.17.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.16.0"
+    let [<Literal>] Version = "0.17.0"
--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,6 +1,10 @@
+#### 0.17.0 - 31.05.2015
+
+* Completion filtering - https://github.com/fsharp/FSharp.AutoComplete/pull/10
+
 #### 0.16.0 - 28.05.2015
 
-*  Implement multiple unsaved file checking - https://github.com/fsharp/FSharp.AutoComplete/pull/8
+* Implement multiple unsaved file checking - https://github.com/fsharp/FSharp.AutoComplete/pull/8
 
 #### 0.15.0 - 20.05.2015
 
