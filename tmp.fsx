2df7bc911839ea906c72cb5ce6b8abff5b07bc6d
Robin Neatherway <robin.neatherway@gmail.com>
2015-08-07 23:48:17 +0100

    Remove incorrectly added files

--- tmp.fsx
+++
@@ -1,8 +0,0 @@
-open System.IO
-
-let fsw = new FileSystemWatcher()
-fsw.Path <- __SOURCE_DIRECTORY__ + "/FsAutoComplete"
-//fsw.Filter <- Path.GetFileName file
-fsw.Filter <- "*.fsproj"
-fsw.Changed.Add(fun x -> printfn "Project file '%s' changed in '%A' way" x.Name x.ChangeType)
-fsw.EnableRaisingEvents <- true
