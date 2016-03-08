490aa24c5f0826ec646356c06b24f603de411d72
Robin Neatherway <robin.neatherway@gmail.com>
2016-03-08 21:40:16 +0000

    Update formatting code and test output

--- src/FsAutoComplete.Core/TipFormatter.fs
+++ src/FsAutoComplete.Core/TipFormatter.fs
@@ -17,36 +17,36 @@ let private buildFormatComment cmt  =
   // files, but I'm not sure whether these are available on Mono
   | _ -> ""
 
-let formatTip tip = 
-  match tip with
-  | FSharpToolTipText tips -> tips |> Seq.where (function
-                                                 | FSharpToolTipElement.Single _ | FSharpToolTipElement.Group _ -> true
-                                                 | _ -> false)
-                                   |>  Seq.fold (fun acc t -> match t with
-                                                              | FSharpToolTipElement.Single (it, comment) -> [(it, comment |> buildFormatComment)]::acc
-                                                              | FSharpToolTipElement.Group (items) -> (items |> List.map (fun (it, comment) ->  (it, comment |> buildFormatComment) )) :: acc
-                                                              | _ -> acc) []
+let formatTip (FSharpToolTipText tips) = 
+    tips
+    |> Seq.where (function
+                    | FSharpToolTipElement.Single _ | FSharpToolTipElement.Group _ -> true
+                    | _ -> false)
+    |>  Seq.fold (fun acc t -> match t with
+                                | FSharpToolTipElement.Single (it, comment) -> [(it, comment |> buildFormatComment)]::acc
+                                | FSharpToolTipElement.Group (items) -> (items |> List.map (fun (it, comment) ->  (it, comment |> buildFormatComment) )) :: acc
+                                | _ -> acc) []
 
+let extractSignature (FSharpToolTipText tips) =
+    let getSignature (str: string) =
+        let nlpos = str.IndexOfAny([|'\r';'\n'|])
+        let firstLine =
+            if nlpos > 0 then str.[0..nlpos-1]
+            else str
 
+        if firstLine.StartsWith("type ", StringComparison.Ordinal) then
+            let index = firstLine.LastIndexOf("=", StringComparison.Ordinal)
+            if index > 0 then firstLine.[0..index-1]
+            else firstLine
+        else firstLine
 
+    let firstResult x =
+        match x with
+        | FSharpToolTipElement.Single (t, _) when not (String.IsNullOrWhiteSpace t) -> Some t
+        | FSharpToolTipElement.Group gs -> List.tryPick (fun (t, _) -> if not (String.IsNullOrWhiteSpace t) then Some t else None) gs
+        | _ -> None
 
-let f x =
-    match x with
-    | FSharpToolTipElement.Single (t, _) when not (String.IsNullOrWhiteSpace t) -> Some t
-    | FSharpToolTipElement.Group gs -> List.tryPick (fun (t, _) -> if not (String.IsNullOrWhiteSpace t) then Some t else None) gs
-    | _ -> None
-
-let format (str: string) =
-    if str.StartsWith("type ", StringComparison.Ordinal) then
-        let index = str.LastIndexOf("=", StringComparison.Ordinal)
-        if index > 0 then
-            str.[0..index-1]
-        else str
-    else str
-
-let extractSignature tip =
-  match tip with
-  | FSharpToolTipText tips -> tips
-                              |> Seq.tryPick f
-                              |> Option.map format
-                              |> Option.getOrElse ""
+    tips
+    |> Seq.tryPick firstResult
+    |> Option.map getSignature
+    |> Option.getOrElse ""
