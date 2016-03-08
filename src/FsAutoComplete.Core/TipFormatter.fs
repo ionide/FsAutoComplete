95154517b9dd5a1c297b184b60d7480c19467174
Robin Neatherway <robin.neatherway@gmail.com>
2016-03-08 19:13:06 +0000

    Add messy type signature extraction

--- src/FsAutoComplete.Core/TipFormatter.fs
+++ src/FsAutoComplete.Core/TipFormatter.fs
@@ -3,6 +3,7 @@
 // --------------------------------------------------------------------------------------
 module FsAutoComplete.TipFormatter
 
+open System
 open System.Text
 open Microsoft.FSharp.Compiler.SourceCodeServices
 
@@ -26,12 +27,26 @@ let formatTip tip =
                                                               | FSharpToolTipElement.Group (items) -> (items |> List.map (fun (it, comment) ->  (it, comment |> buildFormatComment) )) :: acc
                                                               | _ -> acc) []
 
+
+
+
+let f x =
+    match x with
+    | FSharpToolTipElement.Single (t, _) when not (String.IsNullOrWhiteSpace t) -> Some t
+    | FSharpToolTipElement.Group gs -> List.tryPick (fun (t, _) -> if not (String.IsNullOrWhiteSpace t) then Some t else None) gs
+    | _ -> None
+
+let format (str: string) =
+    if str.StartsWith("type ", StringComparison.Ordinal) then
+        let index = str.LastIndexOf("=", StringComparison.Ordinal)
+        if index > 0 then
+            str.[0..index-1]
+        else str
+    else str
+
 let extractSignature tip =
   match tip with
-  | FSharpToolTipText tips -> tips |> Seq.where (function
-                                                 | FSharpToolTipElement.Single _ | FSharpToolTipElement.Group _ -> true
-                                                 | _ -> false)
-                                   |>  Seq.fold (fun acc t -> match t with
-                                                              | FSharpToolTipElement.Single (it, _) -> [ it ]::acc
-                                                              | FSharpToolTipElement.Group (items) -> (items |> List.map (fun (it, _) ->  it )) :: acc
-                                                              | _ -> acc) []
+  | FSharpToolTipText tips -> tips
+                              |> Seq.tryPick f
+                              |> Option.map format
+                              |> Option.getOrElse ""
