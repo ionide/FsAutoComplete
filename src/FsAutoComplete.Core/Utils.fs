1119b93d26a6262fffa298b528b2c19f30f6e3e5
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2016-12-14 17:33:11 +0100

    Add UnopenedNamespacesResolver

--- src/FsAutoComplete.Core/Utils.fs
+++ src/FsAutoComplete.Core/Utils.fs
@@ -14,6 +14,11 @@ type Pos =
     { Line: int
       Col: int }
 
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module Pos =
+    let make line column = { Line = line; Col = column }
+
+
 type Serializer = obj -> string
 type ProjectFilePath = string
 type SourceFilePath = string
@@ -52,7 +57,14 @@ module Option =
     | None -> defaultValue
     | Some x -> x
 
-  let orElse other option =
+  /// Gets the option if Some x, otherwise the supplied default value.
+  let inline orElse v option =
+    match option with
+    | Some x -> Some x
+    | None -> v
+
+
+  let orElseFun other option =
     match option with
     | None -> other()
     | Some x -> Some x
@@ -97,6 +109,27 @@ module Array =
         | null -> nullArg argName
         | _ -> ()
 
+    /// Optimized arrays equality. ~100x faster than `array1 = array2` on strings.
+    /// ~2x faster for floats
+    /// ~0.8x slower for ints
+    let inline areEqual (xs: 'T []) (ys: 'T []) =
+        match xs, ys with
+        | null, null -> true
+        | [||], [||] -> true
+        | null, _ | _, null -> false
+        | _ when xs.Length <> ys.Length -> false
+        | _ ->
+            let mutable break' = false
+            let mutable i = 0
+            let mutable result = true
+            while i < xs.Length && not break' do
+                if xs.[i] <> ys.[i] then
+                    break' <- true
+                    result <- false
+                i <- i + 1
+            result
+
+
     /// Fold over the array passing the index and element at that index to a folding function
     let foldi (folder: 'State -> int -> 'T -> 'State) (state: 'State) (array: 'T []) =
         checkNonNull "array" array
@@ -107,6 +140,38 @@ module Array =
         for i = 0 to len - 1 do
             state <- folder.Invoke (state, i, array.[i])
         state
+
+    /// Returns all heads of a given array.
+    /// For [|1;2;3|] it returns [|[|1; 2; 3|]; [|1; 2|]; [|1|]|]
+    let heads (array: 'T []) =
+        checkNonNull "array" array
+        let res = Array.zeroCreate<'T[]> array.Length
+        for i = array.Length - 1 downto 0 do
+            res.[i] <- array.[0..i]
+        res
+
+    /// check if subArray is found in the wholeArray starting
+    /// at the provided index
+    let inline isSubArray (subArray: 'T []) (wholeArray:'T []) index =
+        if isNull subArray || isNull wholeArray then false
+        elif subArray.Length = 0 then true
+        elif subArray.Length > wholeArray.Length then false
+        elif subArray.Length = wholeArray.Length then areEqual subArray wholeArray else
+        let rec loop subidx idx =
+            if subidx = subArray.Length then true
+            elif subArray.[subidx] = wholeArray.[idx] then loop (subidx+1) (idx+1)
+            else false
+        loop 0 index
+
+    /// Returns true if one array has another as its subset from index 0.
+    let startsWith (prefix: _ []) (whole: _ []) =
+        isSubArray prefix whole 0
+
+    /// Returns true if one array has trailing elements equal to another's.
+    let endsWith (suffix: _ []) (whole: _ []) =
+        isSubArray suffix whole (whole.Length-suffix.Length)
+
+
 
 [<RequireQualifiedAccess>]
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
