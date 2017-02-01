0cb0a5a8aa7f6aed8f0751079b505b2b0cc1cac4
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2017-02-01 13:59:48 +0100

    Port UnionPatternMatchCaseGenerator.fs

--- src/FsAutoComplete.Core/Utils.fs
+++ src/FsAutoComplete.Core/Utils.fs
@@ -111,6 +111,82 @@ module Async =
             return! binding x
         }
 
+[<Sealed>]
+type AsyncMaybeBuilder () =
+    [<DebuggerStepThrough>]
+    member __.Return value : Async<'T option> = Some value |> async.Return
+
+    [<DebuggerStepThrough>]
+    member __.ReturnFrom value : Async<'T option> = value
+
+    [<DebuggerStepThrough>]
+    member __.ReturnFrom (value: 'T option) : Async<'T option> = async.Return value
+
+    [<DebuggerStepThrough>]
+    member __.Zero () : Async<unit option> =
+        Some () |> async.Return
+
+    [<DebuggerStepThrough>]
+    member __.Delay (f : unit -> Async<'T option>) : Async<'T option> = f ()
+
+    [<DebuggerStepThrough>]
+    member __.Combine (r1, r2 : Async<'T option>) : Async<'T option> =
+        async {
+            let! r1' = r1
+            match r1' with
+            | None -> return None
+            | Some () -> return! r2
+        }
+
+    [<DebuggerStepThrough>]
+    member __.Bind (value: Async<'T option>, f : 'T -> Async<'U option>) : Async<'U option> =
+        async {
+            let! value' = value
+            match value' with
+            | None -> return None
+            | Some result -> return! f result
+        }
+
+    [<DebuggerStepThrough>]
+    member __.Bind (value: 'T option, f : 'T -> Async<'U option>) : Async<'U option> =
+        async {
+            match value with
+            | None -> return None
+            | Some result -> return! f result
+        }
+
+    [<DebuggerStepThrough>]
+    member __.Using (resource : ('T :> IDisposable), body : _ -> Async<_ option>) : Async<_ option> =
+        try body resource
+        finally
+            if not << isNull <| resource then resource.Dispose ()
+
+    [<DebuggerStepThrough>]
+    member x.While (guard, body : Async<_ option>) : Async<_ option> =
+        if guard () then
+            x.Bind (body, (fun () -> x.While (guard, body)))
+        else
+            x.Zero ()
+
+    [<DebuggerStepThrough>]
+    member x.For (sequence : seq<_>, body : 'T -> Async<unit option>) : Async<_ option> =
+        x.Using (sequence.GetEnumerator (), fun enum ->
+            x.While (enum.MoveNext, x.Delay (fun () -> body enum.Current)))
+
+    [<DebuggerStepThrough>]
+    member inline __.TryWith (computation : Async<'T option>, catchHandler : exn -> Async<'T option>) : Async<'T option> =
+            async.TryWith (computation, catchHandler)
+
+    [<DebuggerStepThrough>]
+    member inline __.TryFinally (computation : Async<'T option>, compensation : unit -> unit) : Async<'T option> =
+            async.TryFinally (computation, compensation)
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module AsyncMaybe =
+    let inline liftAsync (async : Async<'T>) : Async<_ option> =
+        async |> Async.map Some
+
+
 [<RequireQualifiedAccess>]
 [<CompilationRepresentation (CompilationRepresentationFlags.ModuleSuffix)>]
 module Array =
@@ -265,3 +341,4 @@ type Path with
 
 let inline debug msg = Printf.kprintf Debug.WriteLine msg
 let inline fail msg = Printf.kprintf Debug.Fail msg
+let asyncMaybe = AsyncMaybeBuilder()
