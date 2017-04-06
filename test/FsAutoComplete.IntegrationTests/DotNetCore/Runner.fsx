9fcdf104e8b6d011990f3b5e145f288efb0e79d1
Robin Neatherway <robin.neatherway@gmail.com>
2017-04-06 09:18:13 +0900

    Run dotnet restore prior to test

--- test/FsAutoComplete.IntegrationTests/DotNetCore/Runner.fsx
+++ test/FsAutoComplete.IntegrationTests/DotNetCore/Runner.fsx
@@ -6,6 +6,8 @@ open System
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 File.Delete "output.json"
 
+Diagnostics.Process.Start("dotnet", "restore sample1/c1")
+
 let p = new FsAutoCompleteWrapper()
 
 p.project "sample1/c1/c1.fsproj"
