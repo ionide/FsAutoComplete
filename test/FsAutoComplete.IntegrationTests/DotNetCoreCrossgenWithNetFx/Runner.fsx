2d6beb6ab356080d9a4bb70040e47e586ae850df
Enrico Sada <enrico@sada.io>
2017-06-20 22:39:58 +0200

    add crossgen netcore+net461

---
+++ test/FsAutoComplete.IntegrationTests/DotNetCoreCrossgenWithNetFx/Runner.fsx
@@ -0,0 +1,31 @@
+#load "../TestHelpers.fsx"
+open TestHelpers
+open System.IO
+open System
+
+Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+File.Delete "output.json"
+
+let doIt () =
+  runProcess __SOURCE_DIRECTORY__ "dotnet" "--info" |> ignore
+
+  use _withNetFxLib = DotnetCli.withNetFxBclAvaiable "4.6.1"
+
+  match runProcess __SOURCE_DIRECTORY__ "dotnet" "restore sample1/c1" with
+  | 0 -> ()
+  | err ->
+    let msg = sprintf "failure during 'dotnet restore sample1/c1' with error %i" err
+    msg |> writeNormalizedOutput "output.json"
+    failwith msg
+
+  let p = new FsAutoCompleteWrapper()
+
+  p.project "sample1/c1/c1.fsproj"
+  p.parse "sample1/c1/Program.fs"
+
+  p.send "quit\n"
+  p.finalOutput ()
+  |> writeNormalizedOutput "output.json"
+
+
+doIt ()
