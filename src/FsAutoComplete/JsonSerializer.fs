6b8879ff2a5ff8f386fb75874923a29429710c3f
Enrico Sada <enrico@sada.io>
2017-07-20 17:14:53 +0200

    refactoring json write only converters to functions

--- src/FsAutoComplete/JsonSerializer.fs
+++ src/FsAutoComplete/JsonSerializer.fs
@@ -8,50 +8,50 @@ open Microsoft.FSharp.Compiler.SourceCodeServices
 
 module JsonSerializer =
 
-  type private FSharpErrorSeverityConverter() =
-    inherit JsonConverter()
+  let fsharpErrorSeverityWriter (writer: JsonWriter) value (serializer : JsonSerializer) =
+    let s =
+        match value with
+        | FSharpErrorSeverity.Error -> "Error"
+        | FSharpErrorSeverity.Warning -> "Warning"
+    serializer.Serialize(writer, s)
 
-    override x.CanConvert(t:System.Type) = t = typeof<FSharpErrorSeverity>
-
-    override x.WriteJson(writer, value, serializer) =
-      match value :?> FSharpErrorSeverity with
-      | FSharpErrorSeverity.Error -> serializer.Serialize(writer, "Error")
-      | FSharpErrorSeverity.Warning -> serializer.Serialize(writer, "Warning")
-
-    override x.ReadJson(_reader, _t, _, _serializer) =
-      raise (System.NotSupportedException())
-
-    override x.CanRead = false
-    override x.CanWrite = true
-
-  type private RangeConverter() =
-    inherit JsonConverter()
-
-    override x.CanConvert(t:System.Type) = t = typeof<Range.range>
-
-    override x.WriteJson(writer, value, _serializer) =
-      let range = value :?> Range.range
-      writer.WriteStartObject()
-      writer.WritePropertyName("StartColumn")
-      writer.WriteValue(range.StartColumn + 1)
-      writer.WritePropertyName("StartLine")
-      writer.WriteValue(range.StartLine)
-      writer.WritePropertyName("EndColumn")
-      writer.WriteValue(range.EndColumn + 1)
-      writer.WritePropertyName("EndLine")
-      writer.WriteValue(range.EndLine)
-      writer.WriteEndObject()
-
-    override x.ReadJson(_reader, _t, _, _serializer) =
-      raise (System.NotSupportedException())
-
-    override x.CanRead = false
-    override x.CanWrite = true
+  let rangeWriter (writer: JsonWriter) (range: Range.range) (_serializer : JsonSerializer) =
+    writer.WriteStartObject()
+    writer.WritePropertyName("StartColumn")
+    writer.WriteValue(range.StartColumn + 1)
+    writer.WritePropertyName("StartLine")
+    writer.WriteValue(range.StartLine)
+    writer.WritePropertyName("EndColumn")
+    writer.WriteValue(range.EndColumn + 1)
+    writer.WritePropertyName("EndLine")
+    writer.WriteValue(range.EndLine)
+    writer.WriteEndObject()
 
   let private jsonConverters =
-    [|
-     new FSharpErrorSeverityConverter() :> JsonConverter;
-     new RangeConverter() :> JsonConverter
-    |]
+
+    let jsonWriter (f: JsonWriter -> 'T -> JsonSerializer -> unit) =
+        let boxedF (writer: JsonWriter) (v: obj) (serializer : JsonSerializer) =
+            let typed = v :?> 'T
+            f writer typed serializer
+        typeof<'T>, boxedF
+
+    let writeOnlyConverter (converterOfType, f) = 
+        { new JsonConverter() with
+            member x.CanConvert(t:System.Type) = t = converterOfType
+
+            member x.WriteJson(writer, value, serializer) = f writer value serializer
+
+            member x.ReadJson(_reader, _t, _, _serializer) =
+              raise (System.NotSupportedException())
+
+            member x.CanRead = false
+            member x.CanWrite = true }
+
+    let writers =
+        [| jsonWriter fsharpErrorSeverityWriter
+           jsonWriter rangeWriter |]
+
+    writers
+    |> Array.map writeOnlyConverter
 
   let internal writeJson(o: obj) = JsonConvert.SerializeObject(o, jsonConverters)
