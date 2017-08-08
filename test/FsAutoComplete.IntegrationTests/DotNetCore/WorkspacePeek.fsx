25683c6a215dd8c44f3421b533f99155b4a8a987
Enrico Sada <enrico@sada.io>
2017-08-08 15:52:30 +0200

    add test

---
+++ test/FsAutoComplete.IntegrationTests/DotNetCore/WorkspacePeek.fsx
@@ -0,0 +1,22 @@
+#load "../TestHelpers.fsx"
+open TestHelpers
+open System.IO
+open System
+
+Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+let outputJson = "workspacepeek.json"
+File.Delete outputJson
+
+runProcess __SOURCE_DIRECTORY__ "dotnet" "--info" |> ignore
+
+match runProcessCaptureOut __SOURCE_DIRECTORY__ "dotnet" "restore sample1/c1" with
+| NonExitCodeResult data ->
+    data |> processResultLog "failed 'dotnet restore sample1/c1'" |> writeNormalizedOutput outputJson
+| _ ->
+    let p = new FsAutoCompleteWrapper()
+
+    p.workspacepeek (Path.Combine(__SOURCE_DIRECTORY__, "sample1")) 1
+
+    p.send "quit\n"
+    p.finalOutput ()
+    |> writeNormalizedOutput outputJson
