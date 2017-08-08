0658f21e20f631e4d09a439b621f6bf2f816a8b3
Enrico Sada <enrico@sada.io>
2017-08-08 15:52:30 +0200

    add test

---
+++ test/FsAutoComplete.IntegrationTests/MultiProj/WorkspacePeek.fsx
@@ -0,0 +1,17 @@
+#load "../TestHelpers.fsx"
+open TestHelpers
+open System.IO
+open System
+
+Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+File.Delete "workspacepeek.json"
+
+let p = new FsAutoCompleteWrapper()
+
+p.workspacepeek __SOURCE_DIRECTORY__ 2
+
+p.send "quit\n"
+
+p.finalOutput ()
+|> writeNormalizedOutput "workspacepeek.json"
+
