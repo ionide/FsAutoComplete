54d830f590f708bcdf609157866ed7a574d8db0f
Enrico Sada <enrico@sada.io>
2017-08-29 16:50:47 +0200

    Revert "Disable cache test on Windows"
    This reverts commit 45bf4a7255f8856b0164f722a82a17108ae64981.

--- test/FsAutoComplete.IntegrationTests/ProjectCache/Runner.fsx
+++ test/FsAutoComplete.IntegrationTests/ProjectCache/Runner.fsx
@@ -8,29 +8,27 @@ open System
  * A few completions, files and script.
  *)
 
-if Environment.OSVersion.Platform = PlatformID.Win32NT then
-    ()
-else
-    Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
-    let cacheFile = Path.Combine("obj", "fsac.cache")
-    File.Delete "output.json"
-    if File.Exists cacheFile then File.Delete cacheFile
+
+Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+let cacheFile = Path.Combine("obj", "fsac.cache")
+File.Delete "output.json"
+if File.Exists cacheFile then File.Delete cacheFile
 
 
-    let p = new FsAutoCompleteWrapper()
+let p = new FsAutoCompleteWrapper()
 
-    p.project "Test1.fsproj"
-    p.waitForLine()
-    Threading.Thread.Sleep 2000
-    let msg1, time1 = if File.Exists cacheFile then "cache exists\n", File.GetLastWriteTimeUtc cacheFile else "cache doesn't exist\n", DateTime.MinValue
+p.project "Test1.fsproj"
+p.waitForLine()
+Threading.Thread.Sleep 2000
+let msg1, time1 = if File.Exists cacheFile then "cache exists\n", File.GetLastWriteTimeUtc cacheFile else "cache doesn't exist\n", DateTime.MinValue
 
-    File.WriteAllBytes("Test1.fsproj", File.ReadAllBytes "Test1.fsproj")
-    Threading.Thread.Sleep 2000
-    let time2 = if File.Exists cacheFile then File.GetLastWriteTimeUtc cacheFile else DateTime.MinValue
-    let msg2 = if time1 < time2 then "cache updated" else "cache not updated"
+File.WriteAllBytes("Test1.fsproj", File.ReadAllBytes "Test1.fsproj")
+Threading.Thread.Sleep 2000
+let time2 = if File.Exists cacheFile then File.GetLastWriteTimeUtc cacheFile else DateTime.MinValue
+let msg2 = if time1 < time2 then "cache updated" else "cache not updated"
 
-    p.send "quit\n"
-    p.finalOutput ()
-    |> writeNormalizedOutput "output.json"
-    File.AppendAllText("output.json", msg1)
-    File.AppendAllText("output.json", msg2)
+p.send "quit\n"
+p.finalOutput ()
+|> writeNormalizedOutput "output.json"
+File.AppendAllText("output.json", msg1)
+File.AppendAllText("output.json", msg2)
