4335e648f61e271d07a4b94d8641b57b8ef07c79
Enrico Sada <enrico@sada.io>
2017-10-12 00:42:05 +0200

    fix stdio netcore tests

--- test/FsAutoComplete.IntegrationTests/ProjectReload/Runner.fsx
+++ test/FsAutoComplete.IntegrationTests/ProjectReload/Runner.fsx
@@ -8,8 +8,10 @@ open System
  * A few completions, files and script.
  *)
 
+let outputJson = outputJsonForRuntime "output.json"
+
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
-File.Delete "output.json"
+File.Delete outputJson
 
 let p = new FsAutoCompleteWrapper()
 
@@ -20,5 +22,5 @@ File.WriteAllBytes("Test1.fsproj", File.ReadAllBytes "Test1.fsproj")
 Threading.Thread.Sleep 2000
 p.send "quit\n"
 p.finalOutput ()
-|> writeNormalizedOutput "output.json"
+|> writeNormalizedOutput outputJson
 
