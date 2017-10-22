3be048016b2de938c9d4608cae505caaafa8c157
Enrico Sada <enrico@sada.io>
2017-10-22 16:02:15 +0200

    add bug repro

---
+++ test/FsAutoComplete.IntegrationTests/DotNetSdk2.0/MissingSourceFile.fsx
@@ -0,0 +1,31 @@
+#load "../TestHelpers.fsx"
+open TestHelpers
+open System.IO
+open System
+
+Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+let missingfsJson = "missingfs.json"
+File.Delete missingfsJson
+
+let doIt () =
+  let sdkDir = DotnetCli.sdk2Dir ()
+  
+  use _sdk2 = DotnetCli.useSdk sdkDir
+
+  runProcess __SOURCE_DIRECTORY__ "dotnet" "--info" |> ignore
+
+  match runProcessCaptureOut __SOURCE_DIRECTORY__ "dotnet" "restore sample4/c1" with
+  | NonExitCodeResult data ->
+    data |> processResultLog "failed 'dotnet restore sample4/c1'" |> writeNormalizedOutput missingfsJson
+  | _ ->
+    let p = new FsAutoCompleteWrapper()
+
+    p.project "sample4/c1/c1.fsproj"
+    p.parse "sample4/c1/Program.fs"
+    p.parse "sample4/l1/Library.fs"
+
+    p.send "quit\n"
+    p.finalOutput ()
+    |> writeNormalizedOutput missingfsJson
+
+doIt ()
