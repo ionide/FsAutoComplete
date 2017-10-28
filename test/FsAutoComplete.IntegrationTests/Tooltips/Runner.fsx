1ec8cd4d0152f574dc5bcb0cb0cd723782d6160b
Krzysztof Cieslak <krzysztof_cieslak@windowslive.com>
2017-10-29 00:27:30 +0200

    Add more tests

--- test/FsAutoComplete.IntegrationTests/Tooltips/Runner.fsx
+++ test/FsAutoComplete.IntegrationTests/Tooltips/Runner.fsx
@@ -20,6 +20,19 @@ p.tooltip "Script.fsx" "  type ResponseMsg<'T> =" 10 14
 p.tooltip "Script.fsx" "  let funky x = x + 1" 4 10
 p.tooltip "Script.fsx" "let funct (x : CommandResponse.ResponseMsg<_>) = ()" 19 6
 p.tooltip "Script.fsx" "let x = System.DateTime(123L)" 21 21
+p.tooltip "Script.fsx" "type Abcd () =" 24 8
+p.tooltip "Script.fsx" "type Desf =" 27 8
+p.tooltip "Script.fsx" "  member __.b = 234" 28 5
+p.tooltip "Script.fsx" "type Qwer =" 30 8
+p.tooltip "Script.fsx" "type Gjk =" 35 8
+p.tooltip "Script.fsx" "type Gjk =" 35 2
+p.tooltip "Script.fsx" "let (>=>) a b = a + b" 40 7
+p.tooltip "Script.fsx" "let (|Even|Odd|) input = if input % 2 = 0 then Even else Odd" 42 7
+p.tooltip "Script.fsx" "   | Even -> 1" 46 8
+
+
+
+
 
 
 p.send "quit\n"
--- test/FsAutoComplete.IntegrationTests/Tooltips/Script.fsx
+++ test/FsAutoComplete.IntegrationTests/Tooltips/Script.fsx
@@ -19,3 +19,29 @@ module CommandResponse =
 let funct (x : CommandResponse.ResponseMsg<_>) = ()
 
 let x = System.DateTime(123L)
+
+///Sample type
+type Abcd () =
+  member __.a = 123
+
+type Desf =
+  member __.b = 234
+
+type Qwer =
+  | A of string
+  | B of int * float
+  | C
+
+type Gjk =
+  | Q = 1
+  | W = 2
+  | E = 3
+
+let (>=>) a b = a + b
+
+let (|Even|Odd|) input = if input % 2 = 0 then Even else Odd
+
+let TestNumber input =
+   match input with
+   | Even -> 1
+   | Odd -> 0
