2877e39f651a406ba9607bf57e5ca47167763b5e
Enrico Sada <enrico@sada.io>
2017-10-28 14:29:29 +0200

    workaround for https://github.com/fsprojects/Paket/issues/2868

--- build.fsx
+++ build.fsx
@@ -176,12 +176,19 @@ let runall cfg =
       out |> Seq.iter (printfn "%s")
       printfn "Done: %s" (ok.ToString())
 
-    trace "debug log..."
+    trace "apply workaround for bug https://github.com/fsprojects/Paket/issues/2868"
     let includeFile = Path.Combine(__SOURCE_DIRECTORY__ , @".paket\load\net45\IntegrationTests\Http.fs.fsx")
-    trace (sprintf "File '%s' content (begin)" includeFile)
+    trace (sprintf "File '%s' contents:" includeFile)
     File.ReadAllLines(includeFile)
     |> Array.iter (trace)
-    trace "File content (end)"
+    trace "apply fix"
+    File.ReadAllLines(includeFile)
+    |> Array.map (fun s -> s.Replace("../../../../src/FsAutoComplete.Core.VerboseSdkHelper/.paket/load/net45/IntegrationTests/", ""))
+    |> fun lines -> File.WriteAllLines(includeFile, lines)
+    trace (sprintf "File '%s' contents:" includeFile)
+    File.ReadAllLines(includeFile)
+    |> Array.iter (trace)
+    trace "applied workaround"
 
     trace "Running Integration tests..."
     let runOk =
