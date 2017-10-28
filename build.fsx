3a13fc17d3652877a46fd32c5ac32f017f70d886
Enrico Sada <enrico@sada.io>
2017-10-29 00:16:46 +0200

    reenable fsac net, mode http tests

--- build.fsx
+++ build.fsx
@@ -345,7 +345,7 @@ Target "All" id
 "IntegrationTest" ==> "Test"
 
 "IntegrationTestStdioMode" ==> "IntegrationTest"
-// "IntegrationTestHttpMode" ==> "IntegrationTest"
+"IntegrationTestHttpMode" ==> "IntegrationTest"
 "IntegrationTestStdioModeNetCore" =?> ("IntegrationTest", ((environVar "FSAC_TESTSUITE_NETCORE_MODE_STDIO") <> "0"))
 "IntegrationTestHttpModeNetCore" =?> ("IntegrationTest", ((environVar "FSAC_TESTSUITE_NETCORE_MODE_HTTP") <> "0"))
 
