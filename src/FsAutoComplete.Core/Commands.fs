c3d97a14f044cbd0186f0f0f86e7cce09d3bea28
Thierry Bouquain <thierry.bouquain@ucaya.com>
2018-01-16 16:21:28 +0100

    rebase https://github.com/fsharp/FsAutoComplete/pull/270 to last master sources

--- src/FsAutoComplete.Core/Commands.fs
+++ src/FsAutoComplete.Core/Commands.fs
@@ -480,7 +480,7 @@ type Commands (serialize : Serializer) =
 
     member x.FindTypeDeclaration (tyRes : ParseAndCheckResults) (pos: pos) lineStr =
         tyRes.TryFindTypeDeclaration pos lineStr
-        |> x.SerializeResult (Response.findDeclaration, Response.error)
+        |> x.SerializeResult (Response.findTypeDeclaration, Response.error)
         |> x.AsCancellable (Path.GetFullPath tyRes.FileName)
 
     member x.Methods (tyRes : ParseAndCheckResults) (pos: pos) (lines: LineStr[]) =
