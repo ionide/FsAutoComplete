295f451e553527482f5a31dc3a5bb2cf744ffb99
Julien Roncaglia <julien@roncaglia.fr>
2018-02-17 21:23:10 +0100

    ABORT! ABORT!

--- src/FsAutoComplete/FsAutoComplete.Lsp.fs
+++ src/FsAutoComplete/FsAutoComplete.Lsp.fs
@@ -1,5 +1,5 @@
 module FsAutoComplete.Lsp
-(*
+
 module JsonRpc =
     open Newtonsoft.Json
     open Newtonsoft.Json.Linq
@@ -105,9 +105,111 @@ module FSharpLanguageServer =
 *)
 open Argu
 open System
+open OmniSharp.Extensions.LanguageServer.Server
+open Microsoft.Extensions.Logging
+open FSharp.Control.Tasks
+open OmniSharp.Extensions.LanguageServer.Protocol.Models
+open System.IO
+open System.Threading.Tasks
+open OmniSharp.Extensions.LanguageServer.Protocol
+open OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities
+open System.Threading
+open OmniSharp.Extensions.LanguageServer.Protocol.Server.Capabilities
+open OmniSharp.Extensions.JsonRpc
 
 let start (commands: Commands) (args: ParseResults<Options.CLIArguments>) =
     use input = Console.OpenStandardInput()
     use output = Console.OpenStandardOutput()
+    let server = new LanguageServer(input, output, new LoggerFactory());
+
+    let initialize (p: InitializeParams ) =
+        task {
+            match p.RootPath with
+            | null -> ()
+            | rootPath ->
+                let projects = Directory.EnumerateFiles(rootPath, "*.fsproj", SearchOption.AllDirectories)
+                for project in projects do
+                    let! _ = commands.Project project false ignore |> Async.StartAsTask
+                    ()
+                ()
+            ()
+        } :> Task
+
+    let filter = DocumentFilter()
+    filter.Language <- "fsharp"
+    let selector = DocumentSelector(filter)
+
+    server.OnInitialize(InitializeDelegate(initialize)) |> ignore
+(*
+    server.AddHandler(
+        { new IHoverHandler with
+            member this.Handle(param: TextDocumentPositionParams, token: CancellationToken): Task<Hover> =
+                task { return null }
+
+            member this.GetRegistrationOptions(): TextDocumentRegistrationOptions =
+                let opts = TextDocumentRegistrationOptions()
+                let filter = DocumentFilter()
+                filter.Language <- "fsharp"
+                opts.DocumentSelector <- DocumentSelector(filter)
+                opts
+
+            member this.SetCapability(capability: HoverCapability) =
+                ()
+
+        }) |> ignore
+*)
+    let z =
+        { new ITextDocumentSyncHandler with
+            member __.Options with get(): TextDocumentSyncOptions =
+                let opts = TextDocumentSyncOptions()
+                opts.WillSave <- true
+                opts.Change <- TextDocumentSyncKind.Full
+                let saveOpts = SaveOptions()
+                saveOpts.IncludeText <- true
+                opts.Save <- saveOpts
+                opts.OpenClose <- true
+                opts
+            member __.GetTextDocumentAttributes(uri: Uri) =
+                new TextDocumentAttributes(uri, "fsharp")
+           (* member this.SetCapability(capability: SynchronizationCapability) =
+                ()*)
+          interface ICapability<SynchronizationCapability> with
+            member __.SetCapability(_capability: SynchronizationCapability) =
+                ()
+          interface IDidOpenTextDocumentHandler with
+            member __.Handle(_param: DidOpenTextDocumentParams): Task =
+                Task.CompletedTask
+          interface IRegistration<TextDocumentRegistrationOptions> with
+            member __.GetRegistrationOptions() =
+                let opts = TextDocumentRegistrationOptions()
+                opts.DocumentSelector <- selector
+                opts
+          interface IRegistration<TextDocumentSaveRegistrationOptions> with
+            member __.GetRegistrationOptions() =
+                let opts = TextDocumentSaveRegistrationOptions()
+                opts.DocumentSelector <- selector
+                opts
+          interface IRegistration<TextDocumentChangeRegistrationOptions> with
+            member __.GetRegistrationOptions() =
+                let opts = TextDocumentChangeRegistrationOptions()
+                opts.DocumentSelector <- selector
+                opts
+          interface IJsonRpcHandler
+          interface IDidCloseTextDocumentHandler with
+            member this.Handle(param: DidCloseTextDocumentParams): Task =
+                Task.CompletedTask
+          interface IDidSaveTextDocumentHandler with
+            member this.Handle(param: DidSaveTextDocumentParams): Task =
+                Task.CompletedTask
+          interface IDidChangeTextDocumentHandler with
+            member this.Handle(param: DidChangeTextDocumentParams): Task =
+                Task.CompletedTask
+        }
+
+    let t = task {
+        do! server.Initialize()
+        do! server.WaitForExit
+    }
+    t.Wait()
     //FSharpLanguageServer.start input output
     ()
