1b43a2def65bb0cb8ba2997f6d33fb4fcf8b19eb
Julien Roncaglia <julien@roncaglia.fr>
2018-02-18 21:39:33 +0100

    Cleanup

--- src/FsAutoComplete/JsonSerializer.fs
+++ src/FsAutoComplete/JsonSerializer.fs
@@ -7,31 +7,31 @@ open Microsoft.FSharp.Compiler
 open Microsoft.FSharp.Compiler.SourceCodeServices
 open Microsoft.FSharp.Reflection
 
+type OptionConverter() =
+  inherit JsonConverter()
+
+  override x.CanConvert(t) =
+    t.IsGenericType && t.GetGenericTypeDefinition() = typedefof<option<_>>
+
+  override x.WriteJson(writer, value, serializer) =
+    let value =
+        if isNull value then null
+        else
+          let _,fields = FSharpValue.GetUnionFields(value, value.GetType())
+          fields.[0]
+    serializer.Serialize(writer, value)
+
+  override x.ReadJson(reader, t, existingValue, serializer) =
+    let innerType = t.GetGenericArguments().[0]
+    let innerType =
+      if innerType.IsValueType then (typedefof<Nullable<_>>).MakeGenericType([|innerType|])
+      else innerType
+    let value = serializer.Deserialize(reader, innerType)
+    let cases = FSharpType.GetUnionCases(t)
+    if isNull value then FSharpValue.MakeUnion(cases.[0], [||])
+    else FSharpValue.MakeUnion(cases.[1], [|value|])
+
 module private JsonSerializerConverters =
-
-  type OptionConverter() =
-    inherit JsonConverter()
-
-    override x.CanConvert(t) =
-      t.IsGenericType && t.GetGenericTypeDefinition() = typedefof<option<_>>
-
-    override x.WriteJson(writer, value, serializer) =
-      let value =
-          if isNull value then null
-          else
-            let _,fields = FSharpValue.GetUnionFields(value, value.GetType())
-            fields.[0]
-      serializer.Serialize(writer, value)
-
-    override x.ReadJson(reader, t, existingValue, serializer) =
-      let innerType = t.GetGenericArguments().[0]
-      let innerType =
-        if innerType.IsValueType then (typedefof<Nullable<_>>).MakeGenericType([|innerType|])
-        else innerType
-      let value = serializer.Deserialize(reader, innerType)
-      let cases = FSharpType.GetUnionCases(t)
-      if isNull value then FSharpValue.MakeUnion(cases.[0], [||])
-      else FSharpValue.MakeUnion(cases.[1], [|value|])
 
   let fsharpErrorSeverityWriter (writer: JsonWriter) value (serializer : JsonSerializer) =
     let s =
