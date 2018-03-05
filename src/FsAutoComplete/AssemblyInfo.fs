b320362b0a83a7e697066151d4883e00e8bb4682
Alan Barr <alan@alanmbarr.com>
2018-03-05 14:04:31 -0600

    upgrade packages for test and include Hopac for test runner dependency

--- src/FsAutoComplete/AssemblyInfo.fs
+++ src/FsAutoComplete/AssemblyInfo.fs
@@ -7,7 +7,7 @@ open System.Reflection
 [<assembly: AssemblyDescriptionAttribute("A command line tool for interfacing with FSharp.Compiler.Service over a pipe.")>]
 [<assembly: AssemblyVersionAttribute("0.34.0")>]
 [<assembly: AssemblyFileVersionAttribute("0.34.0")>]
-[<assembly: AssemblyMetadataAttribute("githash","5d82a62347d3b0d96e48d32c1cd3bab9aa89df02")>]
+[<assembly: AssemblyMetadataAttribute("githash","b374512e868835b75bee1ec2c2a10c1e0e31b094")>]
 do ()
 
 module internal AssemblyVersionInformation =
@@ -16,4 +16,4 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyDescription = "A command line tool for interfacing with FSharp.Compiler.Service over a pipe."
     let [<Literal>] AssemblyVersion = "0.34.0"
     let [<Literal>] AssemblyFileVersion = "0.34.0"
-    let [<Literal>] AssemblyMetadata_githash = "5d82a62347d3b0d96e48d32c1cd3bab9aa89df02"
+    let [<Literal>] AssemblyMetadata_githash = "b374512e868835b75bee1ec2c2a10c1e0e31b094"
