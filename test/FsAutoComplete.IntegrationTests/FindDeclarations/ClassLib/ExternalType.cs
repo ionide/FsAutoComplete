1600cfb446f892b1b9f71040966d5a0b0b9a2e0b
Thierry Bouquain <thierry.bouquain@ucaya.com>
2018-07-28 19:49:55 +0200

    use dnspy libs to navigate to decompiled files for external libs (#299)

---
+++ test/FsAutoComplete.IntegrationTests/FindDeclarations/ClassLib/ExternalType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLib
+{
+    public class ExternalType
+    {
+        public static string SimpleMethod()
+        {
+            return null;
+        }
+
+        public static string SimpleMethod(string a, string b)
+        {
+            return null;
+        }
+
+        public static string RefParameter(ref IList<string> lst)
+        {
+            lst = null;
+            return null;
+        }
+
+        public static object Generic<T,U>(Dictionary<T, Dictionary<T, U>> dict)
+        {
+            return dict;
+        }
+
+        
+    }
+}
--- test/FsAutoComplete.IntegrationTests/FindDeclarations/Program.fs
+++ test/FsAutoComplete.IntegrationTests/FindDeclarations/Program.fs
@@ -9,6 +9,16 @@ let val3 = testval.Terrific val2
 
 let val4 : FileTwo.NewObjectType = testval
 
+let val5 = ClassLib.ExternalType.SimpleMethod()
+
+let val6 = ClassLib.ExternalType.SimpleMethod("a", "b")
+
+let mutable lst : System.Collections.Generic.IList<string> = null
+
+let val7 = ClassLib.ExternalType.RefParameter(ref lst)
+
+let val8 = ClassLib.ExternalType.Generic(System.Collections.Generic.Dictionary<string, System.Collections.Generic.Dictionary<string, string>>())
+
 [<EntryPoint>]
 let main args =
     printfn "Hello %d" val2
