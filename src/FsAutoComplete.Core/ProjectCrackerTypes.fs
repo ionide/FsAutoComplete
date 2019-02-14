ab3bc0216a6e02deb7970d1ae712b4eeb34fe97b
Enrico Sada <enrico@sada.io>
2019-02-14 22:45:17 +0100

    projects `Sdk="FSharp.NET.Sdk` are not supported anymore
    the `Dotnet.Proj.Info` >= v0.20 has deprecated these
    
    The `FSharp.NET.Sdk` style projects are just for .NET Core Sdk v1 and can be upgrade to normal .NET Sdk projects targeting same .NET Core Sdk v1

--- src/FsAutoComplete.Core/ProjectCrackerTypes.fs
+++ src/FsAutoComplete.Core/ProjectCrackerTypes.fs
@@ -64,7 +64,7 @@ type [<RequireQualifiedAccess>] WorkspaceProjectState =
 
 module ProjectRecognizer =
 
-    let (|NetCoreProjectJson|NetCoreSdk|Net45|Unsupported|) file =
+    let (|NetCoreProjectJson|FSharpNetSdk|NetCoreSdk|Net45|Unsupported|) file =
         //.NET Core Sdk preview3+ replace project.json with fsproj
         //Easy way to detect new fsproj is to check the msbuild version of .fsproj
         //Post preview5 has (`Sdk="FSharp.NET.Sdk;Microsoft.NET.Sdk"`), use that
@@ -74,6 +74,7 @@ module ProjectRecognizer =
         let rec getProjectType lines =
             // post preview5 dropped this, check Sdk field
             let isNetCore (line:string) = line.ToLower().Contains("sdk=")
+            let isFSharpNetSdk (line:string) = line.ToLower().Contains("fsharp.net.sdk")
             match lines with
             | [] ->
                 Unsupported // unsupported project type
@@ -81,7 +82,10 @@ module ProjectRecognizer =
                 if not <| line.Contains("ToolsVersion") && not <| line.Contains("Sdk=") then
                     getProjectType xs
                 else // both net45 and preview3-5 have 'ToolsVersion', > 5 has 'Sdk'
-                    if isNetCore line then NetCoreSdk else Net45
+                    if isNetCore line then
+                        if isFSharpNetSdk line then FSharpNetSdk else NetCoreSdk
+                    else
+                        Net45
         if Path.GetExtension file = ".json" then
             NetCoreProjectJson // dotnet core preview 2 or earlier
         else
