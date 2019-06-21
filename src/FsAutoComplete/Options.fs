a263ddb14593b87c61f787d3c525708c589460af
Matthias Dittrich <matthi.d@gmail.com>
2019-06-21 13:06:10 +0200

    add --attachDebugger

--- src/FsAutoComplete/Options.fs
+++ src/FsAutoComplete/Options.fs
@@ -55,6 +55,7 @@ module Options =
   type CLIArguments =
       | Version
       | [<AltCommandLine("-v")>] Verbose
+      | AttachDebugger
       | [<EqualsAssignment; AltCommandLine("-l")>] Logfile of path:string
       | VFilter of filter:string
       | Commands
@@ -67,6 +68,7 @@ module Options =
               member s.Usage =
                   match s with
                   | Version -> "display versioning information"
+                  | AttachDebugger -> "launch the system debugger and break."
                   | Verbose -> "enable verbose mode"
                   | Logfile _ -> "send verbose output to specified log file"
                   | VFilter _ -> "apply a comma-separated {FILTER} to verbose output"
@@ -82,6 +84,9 @@ module Options =
       match arg with
       | Verbose ->
           Debug.verbose <- true
+      | AttachDebugger ->
+          System.Diagnostics.Debugger.Launch() |> ignore<bool>
+          System.Diagnostics.Debugger.Break()
       | Logfile s ->
           try
             Debug.output <- (IO.File.CreateText(s) :> IO.TextWriter)
