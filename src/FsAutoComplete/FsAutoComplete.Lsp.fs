f00b24933823eb3216509e31230e7a6c91033b8f
Chet Husk <chusk3@gmail.com>
2019-07-12 00:09:28 -0500

    fix codelensresolve callsite to use the same normalization logic

--- src/FsAutoComplete/FsAutoComplete.Lsp.fs
+++ src/FsAutoComplete/FsAutoComplete.Lsp.fs
@@ -1236,7 +1236,7 @@ type FsharpLspServer(commands: Commands, lspClient: FSharpLspClient) =
             async {
                 let pos = FcsRange.mkPos (arg.Range.Start.Line + 1) (arg.Range.Start.Character + 2)
                 let data = arg.Data.Value.ToObject<string[]>()
-                let file = (Uri data.[0]).LocalPath
+                let file = fileUriToLocalPath data.[0]
                 Debug.print "[LSP] CodeLensResolve - Position request: %s at %A" file pos
                 return!
                     match commands.TryGetFileCheckerOptionsWithLinesAndLineStr(file, pos) with
