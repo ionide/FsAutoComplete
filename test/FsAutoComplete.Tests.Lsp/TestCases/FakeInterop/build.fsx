47e2553e6bbc1cc645e08fe6908ae1f154b9cd07
Chet Husk <baronfel@users.noreply.github.com>
2020-03-08 12:28:37 -0500

    add test case for fake interop (#566)

---
+++ test/FsAutoComplete.Tests.Lsp/TestCases/FakeInterop/build.fsx
@@ -0,0 +1,11 @@
+#r "paket:
+nuget Fake.Core.Target //"
+
+#load "./.fake/build.fsx/intellisense.fsx"
+
+open Fake.Core
+
+// Default target
+Target.create "Default" (fun _ ->
+  Trace.trace "Hello World from FAKE"
+)
