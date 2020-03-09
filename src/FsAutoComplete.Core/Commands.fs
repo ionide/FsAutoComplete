7d3cd54c30ad3050c45aebe2790afc907db6671c
Chet Husk <chusk3@gmail.com>
2020-03-08 19:39:47 -0500

    normalize the path we use as the key

--- src/FsAutoComplete.Core/Commands.fs
+++ src/FsAutoComplete.Core/Commands.fs
@@ -96,7 +96,7 @@ type Commands<'analyzer> (serialize : Serializer, backgroundServiceEnabled) =
                         match state.GetProjectOptions n with
                         | Some opts ->
                             let! res = checker.GetBackgroundCheckResultsForFileInProject(n, opts)
-                            fileChecked.Trigger (res, n, -1)
+                            fileChecked.Trigger (res, Utils.normalizePath res.FileName, -1)
                         | _ -> ()
                     with
                     | _ -> ()
