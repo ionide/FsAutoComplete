1008192e3df0e9deaaa30642df85f5207e29c069
Philipp Haider <haider.philipp@fronius.com>
2020-05-27 12:43:47 +0200

    preserve order of params, exceptions und type args in tool tips

--- test/FsAutoComplete.Tests.Lsp/TestCases/Tooltips/Script.fsx
+++ test/FsAutoComplete.Tests.Lsp/TestCases/Tooltips/Script.fsx
@@ -3,3 +3,12 @@ let listOfTuples = [ 1, 2 ]
 let listOfStructTuples = [ struct(1, 2) ]
 let floatThatShouldHaveGenericReportedInTooltip = 1.
 sprintf "asd"
+
+/// <summary>
+/// My super summary
+/// </summary>
+/// <param name="c">foo</param>
+/// <param name="b">bar</param>
+/// <param name="a">baz</param>
+/// <returns></returns>
+let someFunction () = ()
