41b1ac9d58a077aeea9f7d60b399b0a894898789
Chet Husk <chusk3@gmail.com>
2020-11-16 13:53:21 -0600

    move out some conversions

--- src/FsAutoComplete/LspHelpers.fs
+++ src/FsAutoComplete/LspHelpers.fs
@@ -325,7 +325,70 @@ module SigantureData =
 
         if String.IsNullOrEmpty args then typ else args + " -> " + formatType typ
 
+module Structure =
+      /// convert structure scopes to known kinds of folding range.
+    /// this lets commands like 'fold all comments' work sensibly.
+    /// impl note: implemented as an exhaustive match here so that
+    /// if new structure kinds appear we have to handle them.
+    let scopeToKind (scope: Structure.Scope): string option =
+        match scope with
+        | Structure.Scope.Open -> Some FoldingRangeKind.Imports
+        | Structure.Scope.Comment
+        | Structure.Scope.XmlDocComment -> Some FoldingRangeKind.Comment
+        | Structure.Scope.Namespace
+        | Structure.Scope.Module
+        | Structure.Scope.Type
+        | Structure.Scope.Member
+        | Structure.Scope.LetOrUse
+        | Structure.Scope.Val
+        | Structure.Scope.CompExpr
+        | Structure.Scope.IfThenElse
+        | Structure.Scope.ThenInIfThenElse
+        | Structure.Scope.ElseInIfThenElse
+        | Structure.Scope.TryWith
+        | Structure.Scope.TryInTryWith
+        | Structure.Scope.WithInTryWith
+        | Structure.Scope.TryFinally
+        | Structure.Scope.TryInTryFinally
+        | Structure.Scope.FinallyInTryFinally
+        | Structure.Scope.ArrayOrList
+        | Structure.Scope.ObjExpr
+        | Structure.Scope.For
+        | Structure.Scope.While
+        | Structure.Scope.Match
+        | Structure.Scope.MatchBang
+        | Structure.Scope.MatchLambda
+        | Structure.Scope.MatchClause
+        | Structure.Scope.Lambda
+        | Structure.Scope.CompExprInternal
+        | Structure.Scope.Quote
+        | Structure.Scope.Record
+        | Structure.Scope.SpecialFunc
+        | Structure.Scope.Do
+        | Structure.Scope.New
+        | Structure.Scope.Attribute
+        | Structure.Scope.Interface
+        | Structure.Scope.HashDirective
+        | Structure.Scope.LetOrUseBang
+        | Structure.Scope.TypeExtension
+        | Structure.Scope.YieldOrReturn
+        | Structure.Scope.YieldOrReturnBang
+        | Structure.Scope.Tuple
+        | Structure.Scope.UnionCase
+        | Structure.Scope.EnumCase
+        | Structure.Scope.RecordField
+        | Structure.Scope.RecordDefn
+        | Structure.Scope.UnionDefn -> None
 
+    let toFoldingRange (item: Structure.ScopeRange): FoldingRange =
+        let kind = scopeToKind item.Scope
+        // map the collapserange to the foldingRange
+        let lsp = fcsRangeToLsp item.CollapseRange
+        { StartCharacter   = Some lsp.Start.Character
+          StartLine        = lsp.Start.Line
+          EndCharacter     = Some lsp.End.Character
+          EndLine          = lsp.End.Line
+          Kind             = kind }
 
 type PlainNotification= { Content: string }
 
