75b2ca62160b24aa1f66f8bddc6e982dd687cf72
Jan Zalewski <jasiozetantispam@gmail.com>
2022-01-22 22:19:57 +0100

    Test for Negation to substraction

--- src/FsAutoComplete/CodeFixes/NegationToSubtraction.fs
+++ src/FsAutoComplete/CodeFixes/NegationToSubtraction.fs
@@ -8,26 +8,24 @@ open FsAutoComplete
 open FsAutoComplete.LspHelpers
 
 /// a codefix that corrects -<something> to - <something> when negation is not intended
-let fix (getFileLines: GetFileLines): CodeFix =
-  Run.ifDiagnosticByCode
-    (Set.ofList [ "3" ])
-    (fun diagnostic codeActionParams ->
-      asyncResult {
-        let fileName =
-          codeActionParams.TextDocument.GetFilePath() |> Utils.normalizePath
+let fix (getFileLines: GetFileLines) : CodeFix =
+  Run.ifDiagnosticByCode (Set.ofList [ "3" ]) (fun diagnostic codeActionParams ->
+    asyncResult {
+      let fileName =
+        codeActionParams.TextDocument.GetFilePath()
+        |> Utils.normalizePath
 
-        let! lines = getFileLines fileName
+      let! lines = getFileLines fileName
 
-        match walkForwardUntilCondition lines (inc lines diagnostic.Range.End) (fun ch -> ch = '-') with
-        | Some dash ->
-            return
-              [ { SourceDiagnostic = Some diagnostic
-                  Title = "Use subtraction instead of negation"
-                  File = codeActionParams.TextDocument
-                  Edits =
-                    [| { Range = { Start = dash; End = dec lines dash }
-                         NewText = "- " } |]
-                  Kind = FixKind.Fix } ]
-        | None -> return []
-      }
-      )
+      match walkForwardUntilCondition lines (inc lines diagnostic.Range.End) (fun ch -> ch = '-') with
+      | Some dash ->
+        return
+          [ { SourceDiagnostic = Some diagnostic
+              Title = "Use subtraction instead of negation"
+              File = codeActionParams.TextDocument
+              Edits =
+                [| { Range = { Start = dash; End = dec lines dash }
+                     NewText = "- " } |]
+              Kind = FixKind.Fix } ]
+      | None -> return []
+    })
