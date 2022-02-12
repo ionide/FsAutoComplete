965b8cc7b7767312c782325a7abdb7169cba2619
Chet Husk <chusk3@gmail.com>
2022-02-12 14:11:49 -0600

    use System.CommandLine for CLI parsing

--- src/FsAutoComplete/Options.fs
+++ src/FsAutoComplete/Options.fs
@@ -9,31 +9,80 @@ open Serilog.Core
 open Serilog.Events
 
 module Options =
-  open Argu
+  open System.CommandLine
+  open System.CommandLine.Parsing
+  open System.CommandLine.Builder
+  open System.Threading.Tasks
 
-  type CLIArguments =
-      | Version
-      | [<AltCommandLine("-v")>] Verbose
-      | AttachDebugger
-      | [<EqualsAssignment; AltCommandLine("-l")>] Logfile of path:string
-      | Filter of filter: string list
-      | [<CustomCommandLine("--wait-for-debugger")>] WaitForDebugger
-      | [<EqualsAssignment; CustomCommandLine("--hostPID")>] HostPID of pid:int
-      | [<CustomCommandLine("--background-service-enabled")>] BackgroundServiceEnabled
-      | [<CustomCommandLine("--project-graph-enabled")>] ProjectGraphEnabled
-      with
-          interface IArgParserTemplate with
-              member s.Usage =
-                  match s with
-                  | Version -> "display versioning information"
-                  | AttachDebugger -> "launch the system debugger and break."
-                  | Verbose -> "enable verbose mode"
-                  | Logfile _ -> "send verbose output to specified log file"
-                  | Filter _ -> "filter out messages that match the provided category"
-                  | WaitForDebugger _ -> "wait for a debugger to attach to the process"
-                  | HostPID _ -> "the Host process ID."
-                  | BackgroundServiceEnabled -> "enable background service"
-                  | ProjectGraphEnabled -> "enable MsBuild ProjectGraph for workspace loading. Experimental."
+  let private setArity arity (o: #Option) = o.Arity <- arity; o
+  let inline private zero x = setArity ArgumentArity.Zero x
+  let inline private one x = setArity ArgumentArity.ExactlyOne x
+  let inline private many x = setArity ArgumentArity.OneOrMore x
+
+  let verboseOption =
+    Option<bool>([|"--verbose"; "-v"|], "enable verbose logging")
+    |> setArity ArgumentArity.Zero
+
+  let attachOption =
+    Option<bool>("--attach-debugger", "launch the system debugger and break")
+    |> zero
+
+  let logFileOption =
+    Option<string>([|"--logfile"; "-l"|], "send verbose output to specified log file")
+    |> one
+
+  let logFilterOption =
+    Option<string[]>("--filter", "filter log messages by category")
+    |> many
+
+  let waitForDebuggerOption =
+    Option<bool>("--wait-for-debugger", "wait for a debugger to attach to the process")
+    |> zero
+
+  let hostPIDOption =
+    Option<int>("--hostPID", "the Host process ID")
+    |> one
+
+  let backgroundServiceOption =
+    Option<bool>("--background-service-enabled", "enable background service")
+    |> zero
+
+  let projectGraphOption =
+    Option<bool>("--project-graph-enabled", "enable MSBuild ProjectGraph for workspace loading. Experimental.")
+    |> zero
+
+  let rootCommand =
+    let rootCommand = RootCommand("An F# LSP server implementation")
+
+    rootCommand.AddOption verboseOption
+    rootCommand.AddOption attachOption
+    rootCommand.AddOption logFileOption
+    rootCommand.AddOption logFilterOption
+    rootCommand.AddOption waitForDebuggerOption
+    rootCommand.AddOption hostPIDOption
+    rootCommand.AddOption backgroundServiceOption
+    rootCommand.AddOption projectGraphOption
+    rootCommand
+
+  let waitForDebugger = Invocation.InvocationMiddleware (fun ctx next ->
+    let waitForDebugger = ctx.ParseResult.HasOption waitForDebuggerOption
+    if waitForDebugger then Debug.waitForDebugger()
+    next.Invoke(ctx)
+  )
+
+  let immediateAttach = Invocation.InvocationMiddleware (fun ctx next ->
+    let attachDebugger = ctx.ParseResult.HasOption attachOption
+    if attachDebugger then System.Diagnostics.Debugger.Launch() |> ignore<bool>
+    next.Invoke(ctx)
+  )
+
+  let serilogFlush = Invocation.InvocationMiddleware (fun ctx next -> task {
+    do! next.Invoke ctx
+    Serilog.Log.CloseAndFlush()
+  })
+
+  let parser =
+    CommandLineBuilder(rootCommand).UseDefaults().AddMiddleware(waitForDebugger).AddMiddleware(immediateAttach).AddMiddleware(serilogFlush).Build()
 
   let isCategory (category: string) (e: LogEvent) =
     match e.Properties.TryGetValue "SourceContext" with
@@ -52,34 +101,23 @@ module Options =
   // will use later when a mapping-style config of { "category": "minLevel" } is established
   let excludeByLevelWhenCategory category level event = isCategory category event || not (hasMinLevel level event)
 
-  let apply (levelSwitch: LoggingLevelSwitch) (logConfig: Serilog.LoggerConfiguration) (args: ParseResults<CLIArguments>) =
+  let apply (levelSwitch: LoggingLevelSwitch) (logConfig: Serilog.LoggerConfiguration) (args: ParseResult) =
+    if args.HasOption verboseOption then levelSwitch.MinimumLevel <- LogEventLevel.Verbose
+    if args.HasOption logFileOption then
+      let logFile = args.GetValueForOption logFileOption
 
-    let applyArg arg =
-      match arg with
-      | Verbose ->
-          levelSwitch.MinimumLevel <- LogEventLevel.Verbose
-          ()
-      | AttachDebugger ->
-          System.Diagnostics.Debugger.Launch() |> ignore<bool>
-      | Logfile s ->
-          try
-            logConfig.WriteTo.Async(fun c -> c.File(path = s, levelSwitch = levelSwitch) |> ignore) |> ignore
-          with
-          | e ->
-            eprintfn "Bad log file: %s" e.Message
-            exit 1
-      | Filter categories ->
-          categories
-          |> List.iter (fun category ->
-            // category is encoded in the SourceContext property, so we filter messages based on that property's value
-            logConfig.Filter.ByExcluding(Func<_,_>(isCategory category)) |> ignore
-          )
-      | Version
-      | WaitForDebugger
-      | BackgroundServiceEnabled
-      | ProjectGraphEnabled
-      | HostPID _  ->
-          ()
+      try
+        logConfig.WriteTo.Async(fun c -> c.File(path = logFile, levelSwitch = levelSwitch) |> ignore) |> ignore
+      with
+      | e ->
+        eprintfn "Bad log file: %s" e.Message
+        exit 1
 
-    args.GetAllResults()
-    |> List.iter applyArg
+    if args.HasOption logFilterOption then
+      let categories = args.GetValueForOption logFilterOption
+      categories
+      |> Array.iter (fun category ->
+        // category is encoded in the SourceContext property, so we filter messages based on that property's value
+        logConfig.Filter.ByExcluding(Func<_,_>(isCategory category)) |> ignore
+      )
+
--- src/FsAutoComplete/Program.fs
+++ src/FsAutoComplete/Program.fs
@@ -1,24 +1,18 @@
 module FsAutoComplete.Program
 
 open System
-open FsAutoComplete.JsonSerializer
-open Argu
 open Serilog
 open Serilog.Core
 open Serilog.Events
 open FsAutoComplete.Logging
+open System.CommandLine
+open System.CommandLine.Parsing
+open System.Threading.Tasks
 
 [<EntryPoint>]
 let entry args =
-
-
-    try
-      let parser = ArgumentParser.Create<Options.CLIArguments>(programName = "fsautocomplete")
-
-      let results = parser.Parse args
-
-      System.Threading.ThreadPool.SetMinThreads(16, 16) |> ignore
-
+    System.Threading.ThreadPool.SetMinThreads(16, 16) |> ignore
+    Options.rootCommand.SetHandler(Func<_,Task>(fun (args: ParseResult) ->
       // default the verbosity to warning
       let verbositySwitch = LoggingLevelSwitch(LogEventLevel.Warning)
       let outputTemplate = "[{Timestamp:HH:mm:ss.fff} {Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}"
@@ -35,27 +29,14 @@ let entry args =
             fun c -> c.Console(outputTemplate = outputTemplate, standardErrorFromLevel = Nullable<_>(LogEventLevel.Verbose), theme = Serilog.Sinks.SystemConsole.Themes.AnsiConsoleTheme.Code) |> ignore
           ) // make it so that every console log is logged to stderr
 
-
-      results.TryGetResult(<@ Options.CLIArguments.WaitForDebugger @>)
-      |> Option.iter (ignore >> Debug.waitForDebugger)
-
-      results.TryGetResult(<@ Options.CLIArguments.Version @>)
-      |> Option.iter (fun _ ->
-          let version = Version.info ()
-          printfn "FsAutoComplete %s (git sha %s)" (version.Version) (version.GitSha)
-          exit 0 )
-
-      Options.apply verbositySwitch logConf results
+      Options.apply verbositySwitch logConf args
 
       let logger = logConf.CreateLogger()
       Serilog.Log.Logger <- logger
       LogProvider.setLoggerProvider (Providers.SerilogProvider.create())
 
-      let backgroundServiceEnabled =
-        results.Contains <@ Options.CLIArguments.BackgroundServiceEnabled @>
-
-      let projectGraphEnabled =
-        results.Contains <@ Options.CLIArguments.ProjectGraphEnabled @>
+      let backgroundServiceEnabled = args.HasOption Options.backgroundServiceOption
+      let projectGraphEnabled = args.HasOption Options.projectGraphOption
 
       let workspaceLoaderFactory =
         if projectGraphEnabled then Ionide.ProjInfo.WorkspaceLoaderViaProjectGraph.Create
@@ -64,14 +45,9 @@ let entry args =
       let toolsPath = Ionide.ProjInfo.Init.init (System.IO.DirectoryInfo Environment.CurrentDirectory) None
       use _compilerEventListener = new Debug.FSharpCompilerEventLogger.Listener()
       let result = FsAutoComplete.Lsp.start backgroundServiceEnabled toolsPath workspaceLoaderFactory
-      Serilog.Log.CloseAndFlush()
-      result
-    with
-    | :? ArguParseException as ex ->
-      printfn "%s" ex.Message
-      match ex.ErrorCode with
-      | ErrorCode.HelpText -> 0
-      | _ -> 1  // Unrecognised arguments
-    | e ->
-      printfn "Server crashing error - %s \n %s" e.Message e.StackTrace
-      3
+
+      Task.FromResult result :> Task
+    ))
+
+    let results = Options.parser.Invoke args
+    results
