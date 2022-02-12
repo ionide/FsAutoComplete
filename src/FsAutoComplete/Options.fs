83ac1cc2c8676810ebf024ba66ab2da1377f8816
Chet Husk <chusk3@gmail.com>
2022-02-12 17:05:36 -0600

    docs for args

--- src/FsAutoComplete/Options.fs
+++ src/FsAutoComplete/Options.fs
@@ -21,9 +21,17 @@ module Options =
   type Rng = { File: string; Start: FSharp.Compiler.Text.pos; End: FSharp.Compiler.Text.pos }
 
   let private setArity arity (o: #Option) = o.Arity <- arity; o
+  /// set option to expect no arguments (e.g a flag-style argument: `--verbose`)
   let inline private zero x = setArity ArgumentArity.Zero x
+  /// set option to expect one argument (e.g a single value: `--foo bar)
   let inline private one x = setArity ArgumentArity.ExactlyOne x
+  /// set option to expect multiple arguments
+  /// (e.g a list of values: `--foo bar baz` or `--foo bar --foo baz` depending on the style)
   let inline private many x = setArity ArgumentArity.OneOrMore x
+
+  /// set option to allow multiple arguments per use of the option flag
+  /// (e.g. `--foo bar baz` is equivalent to `--foo bar --foo baz`)
+  let inline private multipleArgs (x: #Option) = x.AllowMultipleArgumentsPerToken <- true; x
 
   let verboseOption =
     Option<bool>([|"--verbose"; "-v"; "--debug"|], "Enable verbose logging. This is equivalent to --log debug.")
@@ -40,8 +48,9 @@ module Options =
     |> one
 
   let logFilterOption =
-    Option<string[]>("--filter", "Filter logs by category. The category can be seen in the logs inside []. For example: [Compiler].")
+    Option<string[]>([|"--filter"; "--log-filter"|], "Filter logs by category. The category can be seen in the logs inside []. For example: [Compiler].")
     |> many
+    |> fun o -> o.AllowMultipleArgumentsPerToken <- true; o
 
   let waitForDebuggerOption =
     Option<bool>("--wait-for-debugger", "Stop execution on startup until an external debugger to attach to this process")
