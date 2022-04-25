8fe04ccc8e43e8a42ce940eaddcbbea790260696
BooksBaum <15612932+Booksbaum@users.noreply.github.com>
2022-04-25 20:06:42 +0200

    Rename param to match signature code fix (#917)

---
+++ src/FsAutoComplete/CodeFixes/RenameParamToMatchSignature.fs
@@ -0,0 +1,84 @@
+module FsAutoComplete.CodeFix.RenameParamToMatchSignature
+
+open FsToolkit.ErrorHandling
+open FsAutoComplete.CodeFix.Types
+open Ionide.LanguageServerProtocol.Types
+open FsAutoComplete
+open FsAutoComplete.LspHelpers
+open FSharp.Compiler.Syntax
+
+let title name = $"Replace with '%s{name}'"
+/// codefix that renames a parameter to match its signature (specified in fsi file)
+let fix
+  (getParseResultsForFile: GetParseResultsForFile)
+  : CodeFix
+  =
+  Run.ifDiagnosticByCode
+    (Set.ofList [ "3218" ])
+    (fun diagnostic codeActionParams -> asyncResult {
+      let tryGetSigName (msg: string) =
+        // diag message:
+        // > The argument names in the signature 'xxx' and implementation 'yyy' do not match. The argument name from the signature file will be used. This may cause problems when debugging or profiling.
+
+        /// Match exact message
+        /// 
+        /// Issues:
+        /// * fails when sig contains `' and implementation '`
+        /// * only works with english language (-> exact match in english)
+        /// * exact match of message -> fails when diag message should change
+        let tryGetByExactMatch (msg: string) =
+          let head = "The argument names in the signature '"
+          let mid = "' and implementation '"
+          if msg.StartsWith head then
+            match msg.IndexOf mid with
+            | -1 -> None
+            | i -> 
+              msg.Substring(head.Length, i - head.Length)
+              |> Some
+          else
+            None
+        /// Fallback to match between `'`s
+        /// 
+        /// Fails when either sig or impl contains `'` (except at end) (`my'value`)
+        let tryGetBySimpleRegex (msg: string) =
+          let parts = System.Text.RegularExpressions.Regex.Match(msg, ".+'(.+)'.+'(.+)'.+")
+          if parts.Success then
+            parts.Groups[1].Value
+            |> Some
+          else
+            None
+        match tryGetByExactMatch msg with
+        | Some name -> 
+            Some name
+        | None ->
+            tryGetBySimpleRegex msg
+
+      match tryGetSigName diagnostic.Message with
+      | None -> return []
+      | Some sigName ->
+          let sigName = 
+            sigName
+            |> PrettyNaming.AddBackticksToIdentifierIfNeeded
+
+          // replace usages of parameter with new name
+          let fileName = codeActionParams.TextDocument.GetFilePath() |> Utils.normalizePath
+          let fscPos = protocolPosToPos diagnostic.Range.Start
+          let! (tyRes, line, _) = getParseResultsForFile fileName fscPos
+
+          let! (_, usages) = tyRes.TryGetSymbolUseAndUsages fscPos line
+          let edits =
+            // usages are inside local function -> all usages in same doc
+            // `usages` includes parameter position
+            usages
+            |> Array.map (fun usage -> 
+              { Range = fcsRangeToLsp usage.Range; NewText = sigName }
+            )
+
+          return [{
+            File = codeActionParams.TextDocument
+            Title = title sigName
+            Edits = edits
+            Kind = FixKind.Fix
+            SourceDiagnostic = Some diagnostic
+          }]
+    })
--- test/FsAutoComplete.Tests.Lsp/CodeFixTests.fs
+++ test/FsAutoComplete.Tests.Lsp/CodeFixTests.fs
@@ -2,10 +2,15 @@ module FsAutoComplete.Tests.CodeFixTests
 
 open Expecto
 open Helpers
+open System.IO
+open Utils.Utils
+open Utils.TextEdit
 open Utils.ServerTests
 open Utils.CursorbasedTests
 open Ionide.LanguageServerProtocol.Types
 open FsAutoComplete.CodeFix
+open Utils.Server
+open Utils.CursorbasedTests.CodeFix
 
 module private Diagnostics =
   let expectCode code (diags: Diagnostic[]) =
@@ -40,6 +45,15 @@ module CodeFix =
     )
     codeActions
 
+/// `ignore testCaseAsync`
+/// 
+/// Like `testCaseAsync`, but test gets completely ignored.
+/// Unlike `ptestCaseAsync` (pending), this here doesn't even show up in Expecto summary.
+/// 
+/// -> Used to mark issues & shortcomings in CodeFixes, but without any (immediate) intention to fix
+///    (vs. `pending` -> marked for fixing)  
+/// -> ~ uncommenting tests without actual uncommenting
+let itestCaseAsync name test = ()
 
 let private addExplicitTypeToParameterTests state =
   serverTestList (nameof AddExplicitTypeToParameter) state defaultConfigDto None (fun server -> [
@@ -777,7 +791,7 @@ let private convertBangEqualsToInequalityTests state =
         """
   ])
 
-let private ConvertCSharpLambdaToFSharpLambdaTests state =
+let private convertCSharpLambdaToFSharpLambdaTests state =
   serverTestList (nameof ConvertCSharpLambdaToFSharpLambda) state defaultConfigDto None (fun server -> [
     let selectCodeFix = CodeFix.withTitle ConvertCSharpLambdaToFSharpLambda.title
     testCaseAsync "can convert csharp lambda in variable assignment with cursor on input" <|
@@ -801,16 +815,16 @@ let private ConvertCSharpLambdaToFSharpLambdaTests state =
         let x = fun y -> 1 + y
         """
     //ENHANCEMENT: trigger on `=>`
-    // testCaseAsync "can convert csharp lambda in variable assignment with cursor on =>" <|
-    //   CodeFix.check server
-    //     """
-    //     let x = y $0=> 1 + y
-    //     """
-    //     Diagnostics.acceptAll
-    //     selectReplaceCSharpLambdaWithFSharp
-    //     """
-    //     let x = fun y -> 1 + y
-    //     """
+    itestCaseAsync "can convert csharp lambda in variable assignment with cursor on =>" <|
+      CodeFix.check server
+        """
+        let x = y $0=> 1 + y
+        """
+        Diagnostics.acceptAll
+        selectCodeFix
+        """
+        let x = fun y -> 1 + y
+        """
     testCaseAsync "can convert csharp lambda in lambda with parens with cursor on input" <|
       CodeFix.check server
         """
@@ -1575,27 +1589,27 @@ let private removeUnusedOpensTests state =
           ()
         """
     //ENHANCEMENT: detect open in outer scope as unused too
-    // testCaseAsync "can remove used open in outer scope when usage in nested scope has own open" <|
-    //   CodeFix.check server
-    //     """
-    //     open $0System
-    //     module A =
-    //       module B =
-    //         open System
-    //         let x = String.IsNullOrWhiteSpace ""
-    //         ()
-    //       ()
-    //     """
-    //     Diagnostics.acceptAll 
-    //     selectCodeFix
-    //     """
-    //     module A =
-    //       module B =
-    //         open System
-    //         let x = String.IsNullOrWhiteSpace ""
-    //         ()
-    //       ()
-    //     """
+    itestCaseAsync "can remove used open in outer scope when usage in nested scope has own open" <|
+      CodeFix.check server
+        """
+        open $0System
+        module A =
+          module B =
+            open System
+            let x = String.IsNullOrWhiteSpace ""
+            ()
+          ()
+        """
+        Diagnostics.acceptAll 
+        selectCodeFix
+        """
+        module A =
+          module B =
+            open System
+            let x = String.IsNullOrWhiteSpace ""
+            ()
+          ()
+        """
     testCaseAsync "doesn't trigger for used open" <|
       CodeFix.checkNotApplicable server
         """
@@ -1604,6 +1618,391 @@ let private removeUnusedOpensTests state =
         """
         Diagnostics.acceptAll 
         selectCodeFix
+  ])
+
+let private renameParamToMatchSignatureTests state =
+  let selectCodeFix expectedName = CodeFix.withTitle (RenameParamToMatchSignature.title expectedName)
+
+  // requires `fsi` and corresponding `fs` file (and a project!)
+  // -> cannot use untitled doc
+  // -> use existing files, but load with text specified in tests
+  let path = Path.Combine(__SOURCE_DIRECTORY__, @"./TestCases/CodeFixTests/RenameParamToMatchSignature/")
+  let (fsiFile, fsFile) = ("Code.fsi", "Code.fs")
+  let (fsiPath, fsPath) = (Path.Combine(path, fsiFile), Path.Combine(path, fsFile))
+
+  serverTestList (nameof RenameParamToMatchSignature) state defaultConfigDto (Some path) (fun server -> [
+    let checkWithFsi
+      fsiSource
+      fsSourceWithCursor
+      selectCodeFix
+      fsSourceExpected
+      = async {
+        let fsiSource = fsiSource |> Text.trimTripleQuotation
+        let (cursor, fsSource) = 
+          fsSourceWithCursor
+          |> Text.trimTripleQuotation
+          |> Cursor.assertExtractRange
+        let! (fsiDoc, diags) = server |> Server.openDocumentWithText fsiFile fsiSource
+        use fsiDoc = fsiDoc
+        Expect.isEmpty diags "There should be no diagnostics in fsi doc"
+        let! (fsDoc, diags) = server |> Server.openDocumentWithText fsFile fsSource
+        use fsDoc = fsDoc
+
+        do! 
+          checkFixAt
+            (fsDoc, diags)
+            (fsSource, cursor)
+            (Diagnostics.expectCode "3218")
+            selectCodeFix
+            (After (fsSourceExpected |> Text.trimTripleQuotation))
+      }
+
+    testCaseAsync "can rename parameter in F# function" <|
+      checkWithFsi
+        """
+        module Code
+
+        val f: value: int -> int
+        """
+        """
+        module Code
+
+        let f $0v = v + 1
+        """
+        (selectCodeFix "value")
+        """
+        module Code
+
+        let f value = value + 1
+        """
+    testCaseAsync "can rename parameter with backticks in signature in F# function" <|
+      checkWithFsi
+        """
+        module Code
+
+        val f: ``my value``: int -> int
+        """
+        """
+        module Code
+
+        let f $0v = v + 1
+        """
+        (selectCodeFix "``my value``")
+        """
+        module Code
+
+        let f ``my value`` = ``my value`` + 1
+        """
+    testCaseAsync "can rename parameter with backticks in implementation in F# function" <|
+      checkWithFsi
+        """
+        module Code
+
+        val f: value: int -> int
+        """
+        """
+        module Code
+
+        let f ``$0my value`` = ``my value`` + 1
+        """
+        (selectCodeFix "value")
+        """
+        module Code
+
+        let f value = value + 1
+        """
+    testCaseAsync "can rename all usage in F# function" <|
+      checkWithFsi
+        """
+        module Code
+
+        val f: x: int -> value: int -> y: int -> int
+        """
+        """
+        module Code
+
+        let f x $0v y = 
+          let a = v + 1
+          let b = v * v
+          let v = a + b
+          v + x * y
+        """
+        (selectCodeFix "value")
+        """
+        module Code
+
+        let f x value y = 
+          let a = value + 1
+          let b = value * value
+          let v = a + b
+          v + x * y
+        """
+    testCaseAsync "can rename parameter with type in F# function" <|
+      checkWithFsi
+        """
+        module Code
+
+        val f: value: int -> int
+        """
+        """
+        module Code
+
+        let f ($0v: int) = v + 1
+        """
+        (selectCodeFix "value")
+        """
+        module Code
+
+        let f (value: int) = value + 1
+        """
+    testCaseAsync "can rename parameter in constructor" <|
+      checkWithFsi
+        """
+        module Code
+
+        type T =
+          new: value: int -> T
+        """
+        """
+        module Code
+
+        type T($0v: int) =
+          let _ = v + 3
+        """
+        (selectCodeFix "value")
+        """
+        module Code
+
+        type T(value: int) =
+          let _ = value + 3
+        """
+    testCaseAsync "can rename parameter in member" <|
+      checkWithFsi
+        """
+        module Code
+
+        type T =
+          new: unit -> T
+          member F: value: int -> int
+        """
+        """
+        module Code
+
+        type T() =
+          member _.F($0v) = v + 1
+        """
+        (selectCodeFix "value")
+        """
+        module Code
+
+        type T() =
+          member _.F(value) = value + 1
+        """
+    testCaseAsync "can rename parameter with ' in signature in F# function" <|
+      checkWithFsi
+        """
+        module Code
+
+        val f: value': int -> int
+        """
+        """
+        module Code
+
+        let f $0v = v + 1
+        """
+        (selectCodeFix "value'")
+        """
+        module Code
+
+        let f value' = value' + 1
+        """
+    testCaseAsync "can rename parameter with ' in implementation in F# function" <|
+      checkWithFsi
+        """
+        module Code
+
+        val f: value: int -> int
+        """
+        """
+        module Code
+
+        let f $0v' = v' + 1
+        """
+        (selectCodeFix "value")
+        """
+        module Code
+
+        let f value = value + 1
+        """
+    testCaseAsync "can rename parameter with ' (not in last place) in signature in F# function" <|
+      checkWithFsi
+        """
+        module Code
+
+        val f: v'2: int -> int
+        """
+        """
+        module Code
+
+        let f $0value = value + 1
+        """
+        (selectCodeFix "v'2")
+        """
+        module Code
+
+        let f v'2 = v'2 + 1
+        """
+    testCaseAsync "can rename parameter with ' (not in last place) in implementation in F# function" <|
+      checkWithFsi
+        """
+        module Code
+
+        val f: value: int -> int
+        """
+        """
+        module Code
+
+        let f $0v'2 = v'2 + 1
+        """
+        (selectCodeFix "value")
+        """
+        module Code
+
+        let f value = value + 1
+        """
+    testCaseAsync "can rename parameter with multiple ' in signature in F# function" <|
+      checkWithFsi
+        """
+        module Code
+
+        val f: value'v'2: int -> int
+        """
+        """
+        module Code
+
+        let f $0v = v + 1
+        """
+        (selectCodeFix "value'v'2")
+        """
+        module Code
+
+        let f value'v'2 = value'v'2 + 1
+        """
+    testCaseAsync "can rename parameter with multiple ' in implementation in F# function" <|
+      checkWithFsi
+        """
+        module Code
+
+        val f: value: int -> int
+        """
+        """
+        module Code
+
+        let f $0value'v'2 = value'v'2 + 1
+        """
+        (selectCodeFix "value")
+        """
+        module Code
+
+        let f value = value + 1
+        """
+    itestCaseAsync "can handle `' and implementation '` in impl name" <|
+      checkWithFsi
+        """
+        module Code
+
+        val f: value: int -> int
+        """
+        """
+        module Code
+
+        let f $0``sig' and implementation 'impl' do not match`` = ``sig' and implementation 'impl' do not match`` + 1
+        """
+        (selectCodeFix "value")
+        """
+        module Code
+
+        let f value = value + 1
+        """
+    //ENHANCEMENT: correctly detect below. Currently: detects sig name `sig`
+    itestCaseAsync "can handle `' and implementation '` in sig name" <|
+      checkWithFsi
+        """
+        module Code
+
+        val f: ``sig' and implementation 'impl' do not match``: int -> int
+        """
+        """
+        module Code
+
+        let f $0value = value + 1
+        """
+        (selectCodeFix "``sig' and implementation 'impl' do not match``")
+        """
+        module Code
+
+        let f ``sig' and implementation 'impl' do not match`` = ``sig' and implementation 'impl' do not match`` + 1
+        """
+  ])
+
+let private renameUnusedValue state =
+  let config = { defaultConfigDto with UnusedDeclarationsAnalyzer = Some true }
+  serverTestList (nameof RenameUnusedValue) state config None (fun server -> [
+    let selectReplace = CodeFix.ofKind "refactor" >> CodeFix.withTitle RenameUnusedValue.titleReplace
+    let selectPrefix = CodeFix.ofKind "refactor" >> CodeFix.withTitle RenameUnusedValue.titlePrefix
+
+    testCaseAsync "can replace unused self-reference" <|
+      CodeFix.check server
+        """
+        type MyClass() =
+          member $0this.DoAThing() = ()
+        """
+        (Diagnostics.acceptAll)
+        selectReplace
+        """
+        type MyClass() =
+          member _.DoAThing() = ()
+        """
+    testCaseAsync "can replace unused binding" <|
+      CodeFix.check server
+        """
+        let $0six = 6
+        """
+        (Diagnostics.acceptAll)
+        selectReplace
+        """
+        let _ = 6
+        """
+    testCaseAsync "can prefix unused binding" <|
+      CodeFix.check server
+        """
+        let $0six = 6
+        """
+        (Diagnostics.acceptAll)
+        selectPrefix
+        """
+        let _six = 6
+        """
+    testCaseAsync "can replace unused parameter" <|
+      CodeFix.check server
+        """
+        let add one two $0three = one + two
+        """
+        (Diagnostics.acceptAll)
+        selectReplace
+        """
+        let add one two _ = one + two
+        """
+    testCaseAsync "can prefix unused parameter" <|
+      CodeFix.check server
+        """
+        let add one two $0three = one + two
+        """
+        (Diagnostics.log >> Diagnostics.acceptAll)
+        (CodeFix.log >> selectPrefix)
+        """
+        let add one two _three = one + two
+        """
   ])
 
 let private replaceWithSuggestionTests state =
@@ -1721,66 +2120,6 @@ let private resolveNamespaceTests state =
     // * Different open locations of CodeFix and AutoOpen
   ])
 
-let private renameUnusedValue state =
-  let config = { defaultConfigDto with UnusedDeclarationsAnalyzer = Some true }
-  serverTestList (nameof RenameUnusedValue) state config None (fun server -> [
-    let selectReplace = CodeFix.ofKind "refactor" >> CodeFix.withTitle RenameUnusedValue.titleReplace
-    let selectPrefix = CodeFix.ofKind "refactor" >> CodeFix.withTitle RenameUnusedValue.titlePrefix
-
-    testCaseAsync "can replace unused self-reference" <|
-      CodeFix.check server
-        """
-        type MyClass() =
-          member $0this.DoAThing() = ()
-        """
-        (Diagnostics.acceptAll)
-        selectReplace
-        """
-        type MyClass() =
-          member _.DoAThing() = ()
-        """
-    testCaseAsync "can replace unused binding" <|
-      CodeFix.check server
-        """
-        let $0six = 6
-        """
-        (Diagnostics.acceptAll)
-        selectReplace
-        """
-        let _ = 6
-        """
-    testCaseAsync "can prefix unused binding" <|
-      CodeFix.check server
-        """
-        let $0six = 6
-        """
-        (Diagnostics.acceptAll)
-        selectPrefix
-        """
-        let _six = 6
-        """
-    testCaseAsync "can replace unused parameter" <|
-      CodeFix.check server
-        """
-        let add one two $0three = one + two
-        """
-        (Diagnostics.acceptAll)
-        selectReplace
-        """
-        let add one two _ = one + two
-        """
-    testCaseAsync "can prefix unused parameter" <|
-      CodeFix.check server
-        """
-        let add one two $0three = one + two
-        """
-        (Diagnostics.log >> Diagnostics.acceptAll)
-        (CodeFix.log >> selectPrefix)
-        """
-        let add one two _three = one + two
-        """
-  ])
-
 let private useMutationWhenValueIsMutableTests state =
   serverTestList (nameof UseMutationWhenValueIsMutable) state defaultConfigDto None (fun server -> [
     let selectCodeFix = CodeFix.withTitle UseMutationWhenValueIsMutable.title
@@ -1879,7 +2218,6 @@ module private CodeFixHelpers =
   // `src\FsAutoComplete\CodeFixes.fs` -> `FsAutoComplete.CodeFix`
   open Navigation
   open FSharp.Compiler.Text
-  open Utils.TextEdit
 
   let private navigationTests =
     testList (nameof Navigation) [
@@ -2032,7 +2370,7 @@ let z = 4"""
       ]
     ]
 
-  let tests = testList ($"{nameof(FsAutoComplete)}.{nameof FsAutoComplete.CodeFix}") [
+  let tests = testList ($"{nameof FsAutoComplete}.{nameof FsAutoComplete.CodeFix}") [
     navigationTests
   ]
 
@@ -2053,7 +2391,7 @@ let tests state = testList "CodeFix tests" [
   changeRefCellDerefToNotTests state
   changeTypeOfNameToNameOfTests state
   convertBangEqualsToInequalityTests state
-  ConvertCSharpLambdaToFSharpLambdaTests state
+  convertCSharpLambdaToFSharpLambdaTests state
   convertDoubleEqualsToSingleEqualsTests state
   convertInvalidRecordToAnonRecordTests state
   convertPositionalDUToNamedTests state
@@ -2066,6 +2404,7 @@ let tests state = testList "CodeFix tests" [
   removeUnnecessaryReturnOrYieldTests state
   removeUnusedBindingTests state
   removeUnusedOpensTests state
+  renameParamToMatchSignatureTests state
   renameUnusedValue state
   replaceWithSuggestionTests state
   resolveNamespaceTests state
