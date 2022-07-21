81671dad868d0fab019013002dd743ed159ed95c
Krzysztof Cieslak <krzysztof_cieslak@windowslive.com>
2022-07-21 20:00:07 +0200

    Format document

--- src/FsAutoComplete.Core/Commands.fs
+++ src/FsAutoComplete.Core/Commands.fs
@@ -787,13 +787,13 @@ type Commands(checker: FSharpCompilerServiceChecker, state: State, hasAnalyzers:
           |> Async.Sequential
           |> Async.map ignore<unit[]>
 
-        // then parse all files in dependent projects
-        // TODO: Disabled due to performance issues - investigate.
-        // do!
-        //   state.ProjectController.GetDependentProjectsOfProjects([ opts ])
-        //   |> List.map x.CheckProject
-        //   |> Async.Sequential
-        //   |> Async.map ignore<unit[]>
+    // then parse all files in dependent projects
+    // TODO: Disabled due to performance issues - investigate.
+    // do!
+    //   state.ProjectController.GetDependentProjectsOfProjects([ opts ])
+    //   |> List.map x.CheckProject
+    //   |> Async.Sequential
+    //   |> Async.map ignore<unit[]>
 
     }
 
