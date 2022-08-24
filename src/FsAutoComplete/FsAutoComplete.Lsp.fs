18d1734c7fd0a46637f428129bf1e15c63bdd668
Chet Husk <chusk3@gmail.com>
2022-08-23 21:14:46 -0500

    bump dependencies

--- src/FsAutoComplete/FsAutoComplete.Lsp.fs
+++ src/FsAutoComplete/FsAutoComplete.Lsp.fs
@@ -908,7 +908,7 @@ type FSharpLspServer(state: State, lspClient: FSharpLspClient) =
             Capabilities =
               { ServerCapabilities.Default with
                   HoverProvider = Some true
-                  RenameProvider = Some true //Some(U2.First true)
+                  RenameProvider = Some(U2.First true)
                   DefinitionProvider = Some true
                   TypeDefinitionProvider = Some true
                   ImplementationProvider = Some true
