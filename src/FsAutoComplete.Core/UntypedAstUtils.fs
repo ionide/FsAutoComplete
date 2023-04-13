640c0917ec4c393c7201d431b6867c55020bfbf7
Chet Husk <chusk3@gmail.com>
2023-04-13 18:35:08 -0500

    fix formatting

--- src/FsAutoComplete.Core/UntypedAstUtils.fs
+++ src/FsAutoComplete.Core/UntypedAstUtils.fs
@@ -243,8 +243,7 @@ let internal getRangesAtPosition input (r: Position) : Range list =
       addIfInside r
       walkType lhs
       walkType rhs
-    | SynType.FromParseError(r) ->
-      addIfInside r
+    | SynType.FromParseError(r) -> addIfInside r
 
   and walkClause (SynMatchClause(pat, e1, e2, r, _, _)) =
     addIfInside r
