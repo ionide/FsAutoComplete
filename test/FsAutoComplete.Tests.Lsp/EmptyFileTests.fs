0dac7576b8221836bd752836e806df06efc56f1f
Hadrian Tang <hadrianwttang@outlook.com>
2023-08-13 17:59:06 +0800

    fix grammar

--- test/FsAutoComplete.Tests.Lsp/EmptyFileTests.fs
+++ test/FsAutoComplete.Tests.Lsp/EmptyFileTests.fs
@@ -89,7 +89,7 @@ let tests state =
                   let! compilerResults = waitForCompilerDiagnosticsForFile "EmptyFile.fsx" events |> Async.StartChild
 
                   match! compilerResults with
-                  | Ok () -> failtest "should get an F# compiler checking error from an 'c' by itself"
+                  | Ok () -> failtest "should get an F# compiler checking error from a 'c' by itself"
                   | Core.Result.Error errors ->
                     Expect.hasLength errors 1 "should have only an error FS0039: identifier not defined"
                     Expect.exists errors (fun error -> error.Code = Some "39") "should have an error FS0039: identifier not defined"
