4320da50e5133fd9375afecf616ec94dcf613a8f
nojaf <florian.verdonck@outlook.com>
2024-03-07 16:21:44 +0100

    Format code

--- src/FsAutoComplete/CodeFixes/AddTypeAliasToSignatureFile.fs
+++ src/FsAutoComplete/CodeFixes/AddTypeAliasToSignatureFile.fs
@@ -11,182 +11,189 @@ open FsAutoComplete.CodeFix.Types
 open FsAutoComplete
 open FsAutoComplete.LspHelpers
 
-let mkLongIdRange (lid : LongIdent) = lid |> List.map (fun ident -> ident.idRange) |> List.reduce Range.unionRanges
+let mkLongIdRange (lid: LongIdent) = lid |> List.map (fun ident -> ident.idRange) |> List.reduce Range.unionRanges
 
-let (|AllOpenOrHashDirective|_|) (decls : SynModuleSigDecl list) : range option =
-    match decls with
-    | [] -> None
-    | decls ->
+let (|AllOpenOrHashDirective|_|) (decls: SynModuleSigDecl list) : range option =
+  match decls with
+  | [] -> None
+  | decls ->
 
     let allOpenOrHashDirective =
-        decls
-        |> List.forall (
-            function
-            | SynModuleSigDecl.Open _
-            | SynModuleSigDecl.HashDirective _ -> true
-            | _ -> false
-        )
+      decls
+      |> List.forall (function
+        | SynModuleSigDecl.Open _
+        | SynModuleSigDecl.HashDirective _ -> true
+        | _ -> false)
 
     if not allOpenOrHashDirective then
-        None
+      None
     else
-        Some (List.last decls).Range.EndRange
+      Some (List.last decls).Range.EndRange
 
-// TODO: add proper title for code fix
+type SynTypeDefn with
+
+  member x.FullRange =
+    match x with
+    | SynTypeDefn(range = m; trivia = { LeadingKeyword = lk }) -> Range.unionRanges lk.Range m
+
 let title = "Add type alias to signature file"
 
 let codeFixForImplementationFileWithSignature
-    (getProjectOptionsForFile : GetProjectOptionsForFile)
-    (codeFix : CodeFix)
-    (codeActionParams : CodeActionParams)
-    : Async<Result<Fix list, string>>
-    =
-    async {
-        let fileName = codeActionParams.TextDocument.GetFilePath () |> Utils.normalizePath
-        let! project = getProjectOptionsForFile fileName
+  (getProjectOptionsForFile: GetProjectOptionsForFile)
+  (codeFix: CodeFix)
+  (codeActionParams: CodeActionParams)
+  : Async<Result<Fix list, string>> =
+  async {
+    let fileName = codeActionParams.TextDocument.GetFilePath() |> Utils.normalizePath
+    let! project = getProjectOptionsForFile fileName
 
-        match project with
-        | Error _ -> return Ok []
-        | Ok projectOptions ->
+    match project with
+    | Error _ -> return Ok []
+    | Ok projectOptions ->
 
-        let signatureFile = String.Concat (fileName, "i")
-        let hasSig = projectOptions.SourceFiles |> Array.contains signatureFile
+      let signatureFile = String.Concat(fileName, "i")
+      let hasSig = projectOptions.SourceFiles |> Array.contains signatureFile
 
-        if not hasSig then
-            return Ok []
-        else
-            return! codeFix codeActionParams
-    }
+      if not hasSig then
+        return Ok []
+      else
+        return! codeFix codeActionParams
+  }
 
 let fix
-    (getProjectOptionsForFile : GetProjectOptionsForFile)
-    (getParseResultsForFile : GetParseResultsForFile)
-    : CodeFix
-    =
-    codeFixForImplementationFileWithSignature
-        getProjectOptionsForFile
-        (fun (codeActionParams : CodeActionParams) ->
-            asyncResult {
-                let fileName = codeActionParams.TextDocument.GetFilePath () |> Utils.normalizePath
-                // The converted LSP start position to an FCS start position.
-                let fcsPos = protocolPosToPos codeActionParams.Range.Start
-                // The syntax tree and typed tree, current line and sourceText of the current file.
-                let! (parseAndCheckResults : ParseAndCheckResults, _line : string, sourceText : IFSACSourceText) =
-                    getParseResultsForFile fileName fcsPos
+  (getProjectOptionsForFile: GetProjectOptionsForFile)
+  (getParseResultsForFile: GetParseResultsForFile)
+  : CodeFix =
+  codeFixForImplementationFileWithSignature getProjectOptionsForFile (fun (codeActionParams: CodeActionParams) ->
+    asyncResult {
+      let fileName = codeActionParams.TextDocument.GetFilePath() |> Utils.normalizePath
+      // The converted LSP start position to an FCS start position.
+      let fcsPos = protocolPosToPos codeActionParams.Range.Start
+      // The syntax tree and typed tree, current line and sourceText of the current file.
+      let! (parseAndCheckResults: ParseAndCheckResults, _line: string, sourceText: IFSACSourceText) =
+        getParseResultsForFile fileName fcsPos
 
-                let typeDefnInfo =
-                    (fcsPos, parseAndCheckResults.GetParseResults.ParseTree)
-                    ||> ParsedInput.tryPick (fun _path node ->
-                        match node with
-                        | SyntaxNode.SynTypeDefn (SynTypeDefn (
-                            typeInfo = SynComponentInfo (longId = [ typeIdent ])
-                            typeRepr = SynTypeDefnRepr.Simple (simpleRepr = SynTypeDefnSimpleRepr.TypeAbbrev _)
-                            range = m
-                            trivia = trivia)) when (Range.rangeContainsPos m fcsPos) ->
-                            let mFull = Range.unionRanges trivia.LeadingKeyword.Range m
-                            Some (typeIdent, mFull)
-                        | _ -> None
-                    )
+      let typeDefnInfo =
+        (fcsPos, parseAndCheckResults.GetParseResults.ParseTree)
+        ||> ParsedInput.tryPick (fun _path node ->
+          match node with
+          | SyntaxNode.SynTypeDefn(SynTypeDefn(
+              typeInfo = SynComponentInfo(longId = [ typeIdent ])
+              typeRepr = SynTypeDefnRepr.Simple(simpleRepr = SynTypeDefnSimpleRepr.TypeAbbrev _)
+              trivia = trivia) as tdn) when (Range.rangeContainsPos tdn.FullRange fcsPos) ->
+            let mFull = Range.unionRanges trivia.LeadingKeyword.Range tdn.FullRange
+            Some(typeIdent, mFull)
+          | _ -> None)
 
-                match typeDefnInfo with
+      match typeDefnInfo with
+      | None -> return []
+      | Some(typeName, mTypeDefn) ->
+
+        match parseAndCheckResults.TryGetSymbolUseFromIdent sourceText typeName with
+        | None -> return []
+        | Some typeSymbolUse ->
+
+          match typeSymbolUse.Symbol with
+          | :? FSharpEntity as entity ->
+            let isPartOfSignature =
+              match entity.SignatureLocation with
+              | None -> false
+              | Some sigLocation -> Utils.isSignatureFile sigLocation.FileName
+
+            if isPartOfSignature then
+              return []
+            else
+
+              let implFilePath = codeActionParams.TextDocument.GetFilePath()
+              let sigFilePath = $"%s{implFilePath}i"
+              let sigFileName = Utils.normalizePath sigFilePath
+
+              let sigTextDocumentIdentifier: TextDocumentIdentifier =
+                { Uri = $"%s{codeActionParams.TextDocument.Uri}i" }
+
+              let! (sigParseAndCheckResults: ParseAndCheckResults, _sigLine: string, sigSourceText: IFSACSourceText) =
+                getParseResultsForFile sigFileName (Position.mkPos 1 0)
+
+              let parentSigLocation =
+                entity.DeclaringEntity
+                |> Option.bind (fun parentEntity ->
+                  match parentEntity.SignatureLocation with
+                  | Some sigLocation when Utils.isSignatureFile sigLocation.FileName -> Some sigLocation
+                  | _ -> None)
+
+              match parentSigLocation with
+              | None -> return []
+              | Some parentSigLocation ->
+
+                // Find a good location to insert the type alias
+                let insertText =
+                  (parentSigLocation.Start, sigParseAndCheckResults.GetParseResults.ParseTree)
+                  ||> ParsedInput.tryPick (fun _path node ->
+                    match node with
+                    | SyntaxNode.SynModuleOrNamespaceSig(SynModuleOrNamespaceSig(longId = longId; decls = decls))
+                    | SyntaxNode.SynModuleSigDecl(SynModuleSigDecl.NestedModule(
+                      moduleInfo = SynComponentInfo(longId = longId); moduleDecls = decls)) ->
+                      let mSigName = mkLongIdRange longId
+
+                      // `parentSigLocation` will only contain the single identifier in case a module is prefixed with a namespace.
+                      if not (Range.rangeContainsRange mSigName parentSigLocation) then
+                        None
+                      else
+
+                        let aliasText =
+                          let text = sourceText.GetSubTextFromRange mTypeDefn
+
+                          if not (text.StartsWith("and", StringComparison.Ordinal)) then
+                            text
+                          else
+                            String.Concat("type", text.Substring 3)
+
+                        match decls with
+                        | [] ->
+                          match node with
+                          | SyntaxNode.SynModuleOrNamespaceSig nm ->
+                            Some(nm.Range.EndRange, String.Concat("\n\n", aliasText))
+
+                          | SyntaxNode.SynModuleSigDecl(SynModuleSigDecl.NestedModule(
+                              range = mNested
+                              trivia = { ModuleKeyword = Some mModule
+                                         EqualsRange = Some mEquals })) ->
+                            let moduleEqualsText =
+                              sigSourceText.GetSubTextFromRange(Range.unionRanges mModule mEquals)
+                            // Can this grabbed from configuration?
+                            let indent = "    "
+
+                            Some(mNested, String.Concat(moduleEqualsText, "\n", indent, aliasText))
+                          | _ -> None
+                        | AllOpenOrHashDirective mLastDecl -> Some(mLastDecl, String.Concat("\n\n", aliasText))
+                        | decls ->
+
+                          decls
+                          // Skip open statements
+                          |> List.tryFind (function
+                            | SynModuleSigDecl.Open _ -> false
+                            | _ -> true)
+                          |> Option.map (fun mdl ->
+                            let offset =
+                              if mdl.Range.StartColumn = 0 then
+                                String.Empty
+                              else
+                                String.replicate mdl.Range.StartColumn " "
+
+                            mdl.Range.StartRange, String.Concat(aliasText, "\n\n", offset))
+                    | _ -> None)
+
+                match insertText with
                 | None -> return []
-                | Some (typeName, mTypeDefn) ->
+                | Some(mInsert, newText) ->
 
-                match parseAndCheckResults.TryGetSymbolUseFromIdent sourceText typeName with
-                | None -> return []
-                | Some typeSymbolUse ->
-
-                match typeSymbolUse.Symbol with
-                | :? FSharpEntity as entity ->
-                    let isPartOfSignature =
-                        match entity.SignatureLocation with
-                        | None -> false
-                        | Some sigLocation -> Utils.isSignatureFile sigLocation.FileName
-
-                    if isPartOfSignature then
-                        return []
-                    else
-
-                    let implFilePath = codeActionParams.TextDocument.GetFilePath ()
-                    let sigFilePath = $"%s{implFilePath}i"
-                    let sigFileName = Utils.normalizePath sigFilePath
-
-                    let sigTextDocumentIdentifier : TextDocumentIdentifier =
-                        {
-                            Uri = $"%s{codeActionParams.TextDocument.Uri}i"
-                        }
-
-                    let! (sigParseAndCheckResults : ParseAndCheckResults,
-                          _sigLine : string,
-                          _sigSourceText : IFSACSourceText) = getParseResultsForFile sigFileName (Position.mkPos 1 0)
-
-                    let parentSigLocation =
-                        entity.DeclaringEntity
-                        |> Option.bind (fun parentEntity ->
-                            match parentEntity.SignatureLocation with
-                            | Some sigLocation when Utils.isSignatureFile sigLocation.FileName -> Some sigLocation
-                            | _ -> None
-                        )
-
-                    match parentSigLocation with
-                    | None -> return []
-                    | Some parentSigLocation ->
-
-                    // Find a good location to insert the type alias
-                    let insertText =
-                        (parentSigLocation.Start, sigParseAndCheckResults.GetParseResults.ParseTree)
-                        ||> ParsedInput.tryPick (fun _path node ->
-                            match node with
-                            | SyntaxNode.SynModuleOrNamespaceSig (SynModuleOrNamespaceSig (
-                                longId = longId ; decls = decls))
-                            | SyntaxNode.SynModuleSigDecl (SynModuleSigDecl.NestedModule (
-                                moduleInfo = SynComponentInfo (longId = longId) ; moduleDecls = decls)) ->
-                                let mSigName = mkLongIdRange longId
-
-                                // `parentSigLocation` will only contain the single identifier in case a module is prefixed with a namespace.
-                                if not (Range.rangeContainsRange mSigName parentSigLocation) then
-                                    None
-                                else
-
-                                let aliasText = sourceText.GetSubTextFromRange mTypeDefn
-
-                                match decls with
-                                | [] -> failwith "todo: empty module"
-                                | AllOpenOrHashDirective mLastDecl ->
-                                    Some (mLastDecl, String.Concat ("\n\n", sourceText.GetSubTextFromRange mTypeDefn))
-                                | decls ->
-
-                                decls
-                                // Skip open statements
-                                |> List.tryFind (
-                                    function
-                                    | SynModuleSigDecl.Open _ -> false
-                                    | _ -> true
-                                )
-                                |> Option.map (fun mdl -> mdl.Range.StartRange, String.Concat (aliasText, "\n\n"))
-                            | _ -> None
-                        )
-
-                    match insertText with
-                    | None -> return []
-                    | Some (mInsert, newText) ->
-
-                    return
-                        [
-                            {
-                                SourceDiagnostic = None
-                                Title = title
-                                File = sigTextDocumentIdentifier
-                                Edits =
-                                    [|
-                                        {
-                                            Range = fcsRangeToLsp mInsert
-                                            NewText = newText
-                                        }
-                                    |]
-                                Kind = FixKind.Fix
-                            }
-                        ]
-                | _ -> return []
-            }
-        )
+                  return
+                    [ { SourceDiagnostic = None
+                        Title = title
+                        File = sigTextDocumentIdentifier
+                        Edits =
+                          [| { Range = fcsRangeToLsp mInsert
+                               NewText = newText } |]
+                        Kind = FixKind.Fix } ]
+          | _ -> return []
+    })
--- test/FsAutoComplete.Tests.Lsp/CodeFixTests/AddTypeAliasToSignatureFileTests.fs
+++ test/FsAutoComplete.Tests.Lsp/CodeFixTests/AddTypeAliasToSignatureFileTests.fs
@@ -8,42 +8,179 @@ open Utils.CursorbasedTests
 open FsAutoComplete.CodeFix
 
 let path =
-    Path.Combine (__SOURCE_DIRECTORY__, @"../TestCases/CodeFixTests/RenameParamToMatchSignature/")
+  Path.Combine(__SOURCE_DIRECTORY__, @"../TestCases/CodeFixTests/RenameParamToMatchSignature/")
 
 let tests state =
-    serverTestList
-        (nameof AddTypeAliasToSignatureFile)
-        state
-        defaultConfigDto
-        (Some path)
-        (fun server ->
-            [
-                let selectCodeFix = CodeFix.withTitle AddTypeAliasToSignatureFile.title
+  serverTestList (nameof AddTypeAliasToSignatureFile) state defaultConfigDto (Some path) (fun server ->
+    let selectCodeFix = CodeFix.withTitle AddTypeAliasToSignatureFile.title
 
-                ftestCaseAsync
-                    "Sample use-case for AddTypeAliasToSignatureFile"
-                    (CodeFix.checkCodeFixInImplementationAndVerifySignature
-                        server
-                        """
+    let test name sigBefore impl sigAfter =
+      testCaseAsync
+        name
+        (CodeFix.checkCodeFixInImplementationAndVerifySignature
+          server
+          sigBefore
+          impl
+          Diagnostics.acceptAll
+          selectCodeFix
+          sigAfter)
+
+    [ test
+        "Sample use-case for AddTypeAliasToSignatureFile"
+        """
 module Foo
 
-open Foo
+open System
 """
-                        """
+        """
 module Foo
 
-open Foo
+open System
 
 type Bar = $0int
 """
-                        Diagnostics.acceptAll
-                        selectCodeFix
-                        """
+        """
 module Foo
 
-open Foo
+open System
 
 type Bar = int
-""")
-            ]
-        )
+"""
+
+      test
+        "place type alias above existing val"
+        """
+module Foo
+
+open System
+
+val a: int
+"""
+        """
+module Foo
+
+open System
+
+let a = 8
+
+type Bar$0 = string
+"""
+        """
+module Foo
+
+open System
+
+type Bar = string
+
+val a: int
+"""
+
+      test
+        "place type alias above existing type"
+        """
+namespace Foo
+
+[<Class>]
+type A =
+    new: unit -> A
+"""
+        """
+namespace Foo
+
+type A() = class end
+
+type $0P = int -> int
+"""
+        """
+namespace Foo
+
+type P = int -> int
+
+[<Class>]
+type A =
+    new: unit -> A
+"""
+
+      test
+        "replace and with type keyword"
+        """
+namespace Foo
+
+open System
+"""
+        """
+namespace Foo
+
+open System
+
+type Foo = class end
+and Bar$0 = string
+"""
+        """
+namespace Foo
+
+open System
+
+type Bar = string
+"""
+
+      test
+        "empty module"
+        """
+namespace Foo
+"""
+        """
+namespace Foo
+
+type $0Foo = int
+"""
+        """
+namespace Foo
+
+type Foo = int
+"""
+
+      test
+        "empty nested module"
+        """
+namespace Foo
+
+module Bar =
+    begin end
+"""
+        """
+namespace Foo
+
+module Bar =
+    type $0Foo = int
+"""
+        """
+namespace Foo
+
+module Bar =
+    type Foo = int
+"""
+
+      test
+        "non empty nested module"
+        """
+namespace Foo
+
+module Bar =
+    val a: int
+"""
+        """
+namespace Foo
+
+module Bar =
+    let a = -9
+    type $0Foo = int
+"""
+        """
+namespace Foo
+
+module Bar =
+    type Foo = int
+
+    val a: int
+""" ])
