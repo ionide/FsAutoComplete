48ea79d1e6490e0be63956ed09a32a761df7fda4
nojaf <florian.verdonck@outlook.com>
2024-03-19 08:44:24 +0100

    Initial test

--- test/FsAutoComplete.Tests.Lsp/CodeFixTests/AddBindingToSignatureFileTests.fs
+++ test/FsAutoComplete.Tests.Lsp/CodeFixTests/AddBindingToSignatureFileTests.fs
@@ -1,20 +1,46 @@
 module private FsAutoComplete.Tests.CodeFixTests.AddBindingToSignatureFileTests
 
+open System.IO
 open Expecto
 open Helpers
 open Utils.ServerTests
 open Utils.CursorbasedTests
 open FsAutoComplete.CodeFix
 
-let tests state =
-  serverTestList (nameof AddBindingToSignatureFile) state defaultConfigDto None (fun server ->
-    [ let selectCodeFix = CodeFix.withTitle AddBindingToSignatureFile.title
+let path =
+  Path.Combine(__SOURCE_DIRECTORY__, @"../TestCases/CodeFixTests/RenameParamToMatchSignature/")
 
-      ftestCaseAsync "first unit test for AddBindingToSignatureFile"
-      <| CodeFix.check
-        server
-        "let a$0 b c = ()"
-        Diagnostics.acceptAll
-        selectCodeFix
-        "let Text replaced by AddBindingToSignatureFile b c = ()"
-    ])
+let tests state =
+  serverTestList (nameof AddBindingToSignatureFile) state defaultConfigDto (Some path) (fun server ->
+    let selectCodeFix = CodeFix.withTitle AddBindingToSignatureFile.title
+
+    let test name sigBefore impl sigAfter =
+      ftestCaseAsync
+        name
+        (CodeFix.checkCodeFixInImplementationAndVerifySignature
+          server
+          sigBefore
+          impl
+          Diagnostics.acceptAll
+          selectCodeFix
+          sigAfter)
+
+    [
+
+      test
+        "Add simple function binding"
+        """
+module Foo
+"""
+        """
+module Foo
+
+let a$0 b = b - 1
+"""
+        """
+module Foo
+
+val a: b: int -> int
+"""
+
+      ])
