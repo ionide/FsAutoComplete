5b9bc1dff756431ddf5d2caf4f85c48141e0b4ee
MrLuje <ogmniluje@gmail.com>
2024-04-16 22:11:56 +0200

    fix unicode characters in F# compiler diagnostic messages (#1265)
    * fix unicode chars in F# compiler diagnostic messages
    
    * fix typo in ShadowedTimeouts focused tests
    
    * fixup! fix unicode chars in F# compiler diagnostic messages
    
    * remove focused tests...
    
    * remove debug prints
    
    Co-authored-by: Jimmy Byrd <jimmybyrd@gmail.com>
    
    ---------
    
    Co-authored-by: Jimmy Byrd <jimmybyrd@gmail.com>

--- test/FsAutoComplete.Tests.Lsp/Program.fs
+++ test/FsAutoComplete.Tests.Lsp/Program.fs
@@ -103,6 +103,7 @@ let lspTests =
             UnusedDeclarationsTests.tests createServer
             EmptyFileTests.tests createServer
             CallHierarchy.tests createServer
+            diagnosticsTest createServer
             ] ]
 
 /// Tests that do not require a LSP server
