a161d96bd0c4574400bf92d582f3f81e27764dad
Edgar Gonzalez <egonzalez@totallymoney.com>
2024-12-26 15:43:45 +0000

    { start..step..finish }

--- test/FsAutoComplete.Tests.Lsp/CodeFixTests/AddMissingSeqTests.fs
+++ test/FsAutoComplete.Tests.Lsp/CodeFixTests/AddMissingSeqTests.fs
@@ -10,6 +10,22 @@ let tests state =
   serverTestList (nameof AddMissingSeq) state defaultConfigDto None (fun server ->
     [ let selectCodeFix = CodeFix.withTitle AddMissingSeq.title
 
+      testCaseAsync "FS3873 — Adds missing seq before { start..step..finish } top level"
+      <| CodeFix.check
+        server
+        "$0{ 1..10..20 }"
+        Diagnostics.acceptAll
+        selectCodeFix
+        "seq { 1..10..20 }"
+
+      testCaseAsync "FS3873 — Adds missing seq before { start..step..finish } binding"
+      <| CodeFix.check
+        server
+        "let xs = $0{ 1..10..20 }"
+        Diagnostics.acceptAll
+        selectCodeFix
+        "let xs = seq { 1..10..20 }"
+
       testCaseAsync "FS3873 — Adds missing seq before { start..finish } top level"
       <| CodeFix.check
         server
@@ -21,10 +37,10 @@ let tests state =
       testCaseAsync "FS3873 — Adds missing seq before { start..finish } binding"
       <| CodeFix.check
         server
-        "let xs = $0{ 1;10 }"
+        "let xs = $0{ 1..10 }"
         Diagnostics.acceptAll
         selectCodeFix
-        "let xs = seq { 1;10 }"
+        "let xs = seq { 1..10 }"
 
       testCaseAsync "FS0740 — Adds missing seq before { x; y } top level"
       <| CodeFix.check
