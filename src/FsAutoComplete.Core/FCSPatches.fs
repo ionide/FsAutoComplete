68ca37034c2fa6b6a8cf64d5b7aabe8e7d1aa0f6
Jimmy Byrd <jimmy.byrd@motivity.net>
2025-02-12 22:00:19 -0500

    Update FCS to 43.9.201

--- src/FsAutoComplete.Core/FCSPatches.fs
+++ src/FsAutoComplete.Core/FCSPatches.fs
@@ -85,9 +85,9 @@ module SyntaxTreeOps =
       | SynExpr.InferredDowncast(e, _)
       | SynExpr.Lazy(e, _)
       | SynExpr.TraitCall(_, _, e, _)
-      | SynExpr.YieldOrReturn(_, e, _)
-      | SynExpr.YieldOrReturnFrom(_, e, _)
-      | SynExpr.DoBang(e, _)
+      | SynExpr.YieldOrReturn(expr = e)
+      | SynExpr.YieldOrReturnFrom(expr = e)
+      | SynExpr.DoBang(expr = e)
       | SynExpr.Fixed(e, _)
       | SynExpr.Paren(e, _, _, _)
       | SynExpr.DotLambda(expr = e) -> walkExpr e
