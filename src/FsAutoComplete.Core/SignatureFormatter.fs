fc526053bd07c46c1f01c85509d16098447edb57
Charlotte Smith <47333659+SoxPopuli@users.noreply.github.com>
2025-03-04 23:23:00 +0000

    Update src/FsAutoComplete.Core/SignatureFormatter.fs
    Co-authored-by: Brian Rourke Boll <brianrourkeboll@users.noreply.github.com>

--- src/FsAutoComplete.Core/SignatureFormatter.fs
+++ src/FsAutoComplete.Core/SignatureFormatter.fs
@@ -54,9 +54,7 @@ module SignatureFormatter =
       | WithGenericArguments x ->
         let parameters =
           x.GenericArguments
-          |> Seq.map ParameterType.getParameterType
-          |> Seq.toList
-          |> List.map ParameterType.displayNameUnAnnotated
+          |> Seq.map (ParameterType.getParameterType >> ParameterType.displayNameUnAnnotated)
           |> String.join ", "
 
         let displayName = x.TypeDefinition.DisplayName
